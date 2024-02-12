7126214e5958c846b8718879ad9d530273cdb73e
Kasumi Null <kasumi.null@yandex.com>
2024-02-12 09:02:32 +0800

    fix =

--- contract/contracts/EngineV1.sol
+++ contract/contracts/EngineV1.sol
@@ -743,7 +743,7 @@ contract EngineV1 is OwnableUpgradeable {
             id := chainid()
         }
 
-        if (id == ARBITRUM_NOVA_CHAINID || id == ARBITRUM_GOERLI_CHAINID || id === ARBITRUM_SEPOLIA_CHAINID) {
+        if (id == ARBITRUM_NOVA_CHAINID || id == ARBITRUM_GOERLI_CHAINID || id == ARBITRUM_SEPOLIA_CHAINID) {
             return ArbSys(ARBSYS_ADDRESS).arbBlockNumber();
         }
 
