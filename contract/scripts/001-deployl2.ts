40823d99f29b8bba44978c955520034e46116b7e
Kasumi Null <kasumi.null@yandex.com>
2024-02-23 09:11:56 +0800

    testnet note

--- contract/scripts/001-deployl2.ts
+++ contract/scripts/001-deployl2.ts
@@ -27,12 +27,10 @@ async function main() {
   console.log('Deploying the L2Token to L2:');
   const L2Token = await ethers.getContractFactory('BaseTokenV1');
   const l2Token = await upgrades.deployProxy(L2Token, [
-    deployer.address,
-    deployer.address,
-    /*
+    // deployer.address,
+    // deployer.address,
     l2Gateway,
     Config.l1TokenAddress,
-    */
   ]);
   // console.log(l2Token);
 
