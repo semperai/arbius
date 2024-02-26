0c53fbbec02cef063fb65b54f8870e95f684845e
Kasumi Null <kasumi.null@yandex.com>
2024-02-26 14:54:41 +0800

    fallback for cache dir

--- miner/src/mc.ts
+++ miner/src/mc.ts
@@ -4,6 +4,9 @@ let c: MiningConfig;
 
 export function initializeMiningConfig(data: MiningConfig) {
   c = data;
+  if (c.cache_path === undefined) {
+    c.cache_path = 'cache';
+  }
 }
 
 export { c }
