37accea344e23235dba6e9e504acd59ed77a66c5
Kasumi Null <kasumi.null@yandex.com>
2024-03-25 19:55:37 +0800

    improve getBlockNumber to work on non-arbitrum chains

--- miner/src/blockchain.ts
+++ miner/src/blockchain.ts
@@ -6,7 +6,6 @@ import { expretry } from './utils';
 
 import EngineArtifact from './artifacts/contracts/V2_EngineV2.sol/V2_EngineV2.json';
 import BaseTokenArtifact from './artifacts/contracts/BaseTokenV1.sol/BaseTokenV1.json';
-// import GovernorArtifact from './artifacts/contracts/GovernorV1.sol/GovernorV1.json';
 // import DelegatedValidator from './artifacts/contracts/DelegatedValidatorV1.sol/DelegatedValidatorV1.json';
 import ArbSysArtifact from './artifacts/@arbitrum/nitro-contracts/src/precompiles/ArbSys.sol/ArbSys.json';
 
@@ -15,9 +14,9 @@ const ARBSYS_ADDR = '0x0000000000000000000000000000000000000064';
 let wallet:   Wallet;
 let arbius:   Contract;
 let token:    Contract;
-// let governor: Contract;
 let solver:   Contract; // this could be be either arbius or delegated validator
 let arbsys:   Contract;
+let chainId: number;
 
 export async function initializeBlockchain() {
   const provider = new ethers.providers.JsonRpcProvider(c.blockchain.rpc_url!);
@@ -25,8 +24,13 @@ export async function initializeBlockchain() {
 
   arbius   = new Contract(Config.v2_engineAddress,    EngineArtifact.abi,    wallet);
   token    = new Contract(Config.v2_baseTokenAddress, BaseTokenArtifact.abi, wallet);
-  // governor = new Contract(Config.governorAddress,  GovernorArtifact.abi,  wallet);
   arbsys   = new Contract(ARBSYS_ADDR,             ArbSysArtifact.abi,    wallet);
+
+  let attemptToGetChainId = await expretry(async () => (await wallet.provider.getNetwork()).chainId);
+  if (! attemptToGetChainId) {
+    throw new Error('Could not get chainId');
+  }
+  chainId = attemptToGetChainId;
 
   if (! c.blockchain.use_delegated_validator) {
     solver = new Contract(Config.v2_engineAddress,    EngineArtifact.abi,    wallet);
@@ -35,15 +39,22 @@ export async function initializeBlockchain() {
   }
 }
 
-// TODO check if we are on nova network, if so use this, otherwise block.number
+// arb.blockNumber is only available on Arbitrum chains
 export async function getBlockNumber() {
-  return await expretry(async () => await arbsys.arbBlockNumber());
+  switch (chainId) {
+    case 0xa4ba:
+    case 0x66eed:
+    case 0x66eee:
+      return await expretry(async () => await arbsys.arbBlockNumber());
+    default:
+      return await expretry(async () => await wallet.provider.getBlockNumber());
+  }
 }
 
 export {
   wallet,
   arbius,
   token,
-  // governor,
   solver,
+  chainId,
 }
