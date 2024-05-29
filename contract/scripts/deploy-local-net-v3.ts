67fd4a54fa7f7c5b2a9d2f94b4c5dcbbb9e6bd90
Kasumi Null <kasumi.null@yandex.com>
2024-05-30 02:08:06 +0800

    v3 deploy scripts

---
+++ contract/scripts/deploy-local-net-v3.ts
@@ -0,0 +1,32 @@
+import { ethers, upgrades } from "hardhat";
+import { getL2Network } from '@arbitrum/sdk';
+import { V2EngineV2 as EngineV2 } from '../typechain/V2EngineV2';
+import { V2EngineV3 as EngineV3 } from '../typechain/V2EngineV3';
+import * as fs from 'fs'
+import Config from './config.local.json';
+import 'dotenv/config';
+
+
+async function main() {
+  const signers = await ethers.getSigners();
+  const deployer   = signers[0];
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const EngineV3 = await ethers.getContractFactory("V2_EngineV3");
+
+  const engine = await upgrades.upgradeProxy(Config.v2_engineAddress, EngineV3) as EngineV3;
+  console.log("Engine upgraded");
+
+  /*
+  await (await engine
+    .connect(deployer)
+    .setStartBlockTime(startTime)
+  ).wait();
+  console.log(`Start block time set to ${new Date((startTime) * 1000).toString()}`);
+  */
+
+  process.exit(0)
+}
+
+main();
