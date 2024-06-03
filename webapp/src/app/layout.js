dc27d81f6bb5c4f00b12086d323fa6de47f9081d
shayan-cyber <debroyshayan@gmail.com>
2024-06-03 16:10:21 +0530

    UPD:MLS_Models_Partners_Mobile_responsiveness_added

--- webapp/src/app/layout.js
+++ webapp/src/app/layout.js
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header/> */}
+        <Header/>
         {children}
-        {/* <Footer/> */}
+        <Footer/>
       </body>
     </html>
   );
