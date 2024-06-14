9c5c3d9b17367817c671631393b3dfc67dc0d309
shayan-cyber <debroyshayan@gmail.com>
2024-06-14 11:54:58 +0530

    UPD:AIUS_slider

--- webapp/src/app/stake/aius/Stake.jsx
+++ webapp/src/app/stake/aius/Stake.jsx
@@ -1,11 +1,11 @@
 "use client"
-import React,{useState} from "react"
+import React, { useState } from "react"
 import arbius_logo_without_name from '@/app/assets/images/arbius_logo_without_name.png'
 import Image from "next/image"
 import ReactSlider from 'react-slider'
-export default function Stake(){
-    const [sliderValue,setSliderValue]=useState(0)
-    return(
+export default function Stake() {
+    const [sliderValue, setSliderValue] = useState(0)
+    return (
         <div>
             <div className="bg-white-background 2xl:h-[480px] lg:h-[535px] h-auto stake-box-shadow rounded-2xl px-8 2xl:pt-10 lg:pt-14 pb-8 pt-8 box-border">
                 <div>
@@ -17,12 +17,12 @@ export default function Stake(){
                         <div className="border border-[#2F2F2F] rounded-3xl flex items-center">
                             <div className="bg-stake-input flex items-center gap-2 justify-center rounded-l-3xl  p-2 box-border">
                                 <div className="bg-white-background w-[30px] h-[30px] rounded-[50%] flex items-center justify-center ">
-                                   <Image src={arbius_logo_without_name} width={15} alt="arbius"/>
+                                    <Image src={arbius_logo_without_name} width={15} alt="arbius" />
                                 </div>
                                 <p className="pr- text-aius lato-bold text-[15px]">AIUS</p>
                             </div>
                             <div className="w-[94%]">
-                                <input className="w-[100%] border-0 outline-none rounded-r-3xl p-2 lato-bold text-[15px]" type="number" placeholder="0.0"/>
+                                <input className="w-[100%] border-0 outline-none rounded-r-3xl p-2 lato-bold text-[15px]" type="number" placeholder="0.0" />
                             </div>
                         </div>
                     </div>
@@ -31,10 +31,12 @@ export default function Stake(){
                     <p className="mt-8 mb-8 text-[22px] lato-bold  text-stake">Locking for {sliderValue} months for 0.0 AIUS voting power.</p>
                     <div className="mb-10">
                         <div className="mb-8">
-                            <ReactSlider
+                            {/* <ReactSlider
                                 min={0}
                                 max={24}
                                 step={1}
+                                marks={[0, 4, 8, 16,12, 20, 24]}
+                                markClassName={"w-[4px] h-[8px] bg-[#000000] ml-0"}
                                 onAfterChange={(value, index) =>
                                     {
                                         setSliderValue(value)
@@ -44,17 +46,42 @@ export default function Stake(){
                                 thumbClassName={`example-thumb w-[28px] h-[28px] ${!sliderValue?"ml-[-5px]":"ml-[5px]"} bg-thumb cursor-pointer rounded-[50%] flex items-center justify-center border-0 mt-[-14px] outline-none`}
                                 // trackClassName="bg-[#000000] h-[2px] w-[100%]"
                                 renderThumb={(props, state) => <div {...props}></div>}
+                            /> */}
+
+                            <ReactSlider
+                                className=" text-original-white border-b border-4 border-[#ECECEC] rounded-2xl mb-32"
+                                thumbClassName={`example-thumb w-[28px] h-[28px] ${sliderValue === 0 ? "ml-[-8px]" : "ml-[-4px]"} bg-thumb cursor-pointer rounded-[50%] flex items-center justify-center border-0 mt-[-14px] outline-none`}
+
+                                markClassName="customSlider-mark"
+                                marks={4}
+                                min={0}
+                                max={24}
+                                defaultValue={0}
+                                value={sliderValue}
+                                onChange={(value) => setSliderValue(value)}
+                                renderMark={(props) => {
+                               
+                                
+                                    
+                                        props.className = "customSlider-mark customSlider-mark-before text-[16px] text-start ml-[0px] w-[16.66%]";
+                                    
+
+
+                                    return <span {...props} >
+                                        <h1>{props.key}</h1>
+                                    </span>;
+                                }}
                             />
                         </div>
-                        <div className="flex justify-between items-center text-[#808080] lato-regular ">
-                          <p>0</p>  
-                          <p>4</p>
-                          <p>8</p>
-                          <p>12</p>
-                          <p>16</p>
-                          <p>20</p>
-                          <p>24</p>
-                        </div>
+                        {/* <div className="flex justify-between items-center text-[#808080] lato-regular ">
+                            <p>0</p>
+                            <p>4</p>
+                            <p>8</p>
+                            <p>12</p>
+                            <p>16</p>
+                            <p>20</p>
+                            <p>24</p>
+                        </div> */}
                     </div>
                 </div>
                 <div className="flex items-center justify-between">
