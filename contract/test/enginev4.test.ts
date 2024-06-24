4de45110e3ab155e2eb635e5ef0941fcbfc7c9bd
beskay <beskay0x@gmail.com>
2024-06-25 00:14:41 +0200

    Update v4 tests to working state

--- contract/test/enginev4.test.ts
+++ contract/test/enginev4.test.ts
@@ -11,7 +11,7 @@ import { V2EngineV4 } from "../typechain/V2EngineV4";
 const TESTCID = '0x1220f4ad8a3bd3189da2ad909ee41148d6893d8c629c410f7f2c7e3fae75aade79c8';
 const TESTBUF = '0x746573740a';
 
-describe("EngineV4 Unit Tests", () => {
+describe("EngineV4 SetUp", () => {
   let signers: SignerWithAddress[];
   // let deployer: Signer;
   let deployer:   SignerWithAddress;
@@ -26,9 +26,10 @@ describe("EngineV4 Unit Tests", () => {
   let newowner:   SignerWithAddress;
 
   let baseToken: BaseToken;
+  let enginev2: V2EngineV2;
   let engine: V2EngineV4;
 
-  beforeEach("Deploy and initialize", async () => {
+  async function deployAndInitialize() {
     signers = await ethers.getSigners();
     deployer   = signers[0];
     user1      = signers[1];
@@ -49,7 +50,7 @@ describe("EngineV4 Unit Tests", () => {
       ethers.constants.AddressZero,
     ])) as BaseToken;
     await baseToken.deployed();
-    // console.log("BaseToken deployed to:", baseToken.address);
+    console.log("BaseToken deployed to: ", baseToken.address);
 
     const V2_EngineV1 = await ethers.getContractFactory(
       "V2_EngineV1"
@@ -69,10 +70,18 @@ describe("EngineV4 Unit Tests", () => {
       await treasury.getAddress(),
     ])) as V2EngineV1 as any;
     await engine.deployed();
+    console.log("engine deployed to: ", engine.address);
     
     // upgrade successive versions
-    engine = await upgrades.upgradeProxy(engine.address, V2_EngineV2) as V2EngineV2 as any;
-    engine = await upgrades.upgradeProxy(engine.address, V2_EngineV3, { call: "initialize" }) as V2EngineV3 as any;
+    enginev2 = await upgrades.upgradeProxy(engine.address, V2_EngineV2) as V2EngineV2 as any;
+
+    // set solution stake amount in v2, since it cant be set in later versions
+    await (await enginev2
+      .connect(deployer)
+      .setSolutionStakeAmount(ethers.utils.parseEther('0.001'))
+    ).wait(); 
+
+    engine = await upgrades.upgradeProxy(enginev2.address, V2_EngineV3, { call: "initialize" }) as V2EngineV3 as any;
     engine = await upgrades.upgradeProxy(engine.address, V2_EngineV4, { call: "initialize" }) as V2EngineV4;
 
     // for testing transfer from here
@@ -93,94 +102,12 @@ describe("EngineV4 Unit Tests", () => {
         .approve(engine.address, ethers.constants.MaxUint256)
       ).wait();
     }
-  });
-
-  // early txs need 0 model fees and 0 task fees so they can mine tokens
-  // later tests will cover fees
-  async function deployBootstrapModel(): Promise<string> {
-    const addr = await user1.getAddress();
-    const fee = ethers.utils.parseEther('0');
-
-    const modelid = await engine.hashModel({
-      addr,
-      fee,
-      rate: ethers.utils.parseEther('0'),
-      cid: TESTCID,
-    }, await user1.getAddress());
-
-    await (await engine
-      .connect(user1)
-      .registerModel(addr, fee, TESTBUF)
-    ).wait();
-
-    return modelid;
-  }
-
-  async function bootstrapTaskParams(modelid: string, feeEth: string = '0') {
-    return {
-      version: BigNumber.from("0"),
-      owner: await user1.getAddress(),
-      model: modelid,
-      fee: ethers.utils.parseEther(feeEth),
-      input: TESTBUF, // normally this would be json but it doesnt matter for testing
-      cid: TESTCID,
-    };
-  }
-
-  async function deployBootstrapValidator(): Promise<string> {
-    // deposit just below the 600k max supply
-    await (await baseToken
-      .connect(deployer)
-      .bridgeMint(engine.address, ethers.utils.parseEther('599990'))
-    ).wait();
-
-    await (await baseToken
-      .connect(deployer)
-      .transfer(await validator1.getAddress(), ethers.utils.parseEther('10'))
-    ).wait();
-
-    await (await engine
-      .connect(validator1)
-      .validatorDeposit(await validator1.getAddress(), ethers.utils.parseEther('10'))
-    ).wait();
-    return await validator1.getAddress();
-  }
-
-  async function deployBootstrapEngineSlashingNotReached(): Promise<void> {
-    // deposit just below the 598000 max supply
-    await (await baseToken
-      .connect(deployer)
-      .bridgeMint(engine.address, ethers.utils.parseEther('597000'))
-    ).wait();
-  }
-
-  async function deployBootstrapEngineSlashingReached(): Promise<void> {
-      // deposit just below the 598000 max supply
-      await (await baseToken
-        .connect(deployer)
-        .bridgeMint(engine.address, ethers.utils.parseEther('597000'))
-      ).wait();
-  }
-
-  async function deployBootstrapTask(modelid: string, submitter?: SignerWithAddress, feeEth?: string): Promise<string> {
-    const taskParams = await bootstrapTaskParams(modelid, feeEth);
-    const taskidReceipt = await (await engine
-      .connect(submitter ?? user1)
-      .submitTask(
-        taskParams.version,
-        taskParams.owner,
-        taskParams.model,
-        taskParams.fee,
-        taskParams.input,
-      )).wait();
-    const taskSubmittedEvent = taskidReceipt.events![0];
-    // @ts-ignore
-    const { id: taskid } = taskSubmittedEvent.args;
-    return taskid;
-  }
+  };
 
   describe("upgrade", () => {
-    it("can validate upgrade", async () => {
+    it.only("can validate upgrade", async () => {
+      await deployAndInitialize();
+
       const V2_EngineV1 = await ethers.getContractFactory('V2_EngineV1');
       const V2_EngineV2 = await ethers.getContractFactory('V2_EngineV2');
       const V2_EngineV3 = await ethers.getContractFactory('V2_EngineV3');
@@ -189,10 +116,6 @@ describe("EngineV4 Unit Tests", () => {
       await upgrades.validateUpgrade(V2_EngineV1, V2_EngineV2);
       await upgrades.validateUpgrade(V2_EngineV2, V2_EngineV3);
       await upgrades.validateUpgrade(V2_EngineV3, V2_EngineV4);
-    });
-
-    it("calls reinitializer to set new params", async() => {
-      // empty for now
     });
   });
 });
--- contract/test/foundry/V2_EngineV4.t.sol
+++ contract/test/foundry/V2_EngineV4.t.sol
@@ -3,16 +3,42 @@ pragma solidity ^0.8.19;
 
 import './BaseTest.sol';
 
+import "contracts/V2_EngineV4.sol";
+import "contracts/BaseTokenV1.sol";
+
 /// @notice Tests are invoked with `npm forge-test` command
 contract EngineV4Test is BaseTest {
+
+    // default test mnemonic used in hardhat tests
+    string constant public mnemonic = "test test test test test test test test test test test junk";
+
+    address deployer = vm.addr(vm.deriveKey(mnemonic, 0));
+    address user1 = vm.addr(vm.deriveKey(mnemonic, 1));
+    address user2 = vm.addr(vm.deriveKey(mnemonic, 2));
+    address validator1 = vm.addr(vm.deriveKey(mnemonic, 3));
+    address validator2 = vm.addr(vm.deriveKey(mnemonic, 4));
+    address validator3 = vm.addr(vm.deriveKey(mnemonic, 5));
+    address validator4 = vm.addr(vm.deriveKey(mnemonic, 6));
+    address treasury = vm.addr(vm.deriveKey(mnemonic, 7));
+    address model1 = vm.addr(vm.deriveKey(mnemonic, 8));
+    address newowner = vm.addr(vm.deriveKey(mnemonic, 9));
+
+    // contracts
+    V2_EngineV4 public engine = V2_EngineV4(0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9);
+    BaseTokenV1 public baseToken = BaseTokenV1(0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0); 
 
     function setUp() public {
         // set up is done in hardhat test file: test/enginev4.test.ts
         
-        // set time
-        //vm.warp((1704067200  / WEEK) * WEEK); // Thu Dec 28 2023 00:00:00 GMT+0000        
+        // todo: implement this in hardhat 
+        // vm.warp((1704067200  / WEEK) * WEEK); // Thu Dec 28 2023 00:00:00 GMT+0000        
     }
 
-      // todo: rewards over 7 years, ve APY, task submitting etc
+    function testSetPaused() public {
+        vm.prank(deployer);
+        engine.setPaused(true);
+    }
+    
+    // todo: rewards over 7 years, ve APY, task submitting etc
 
 }
