372b9444a9767be9b41e217213ec9accce347ddb
Abhishek <munjalabhishek1996@gmail.com>
2024-06-26 10:55:35 +0530

    Removed webapp folder - new next app by WO, leave just the merge of the two sites

--- webapp/src/app/assets/fonts/fonts.js
+++
@@ -1,7 +0,0 @@
-import {Lato} from 'next/font/google'
-export const lato = Lato({
-    subsets: ['latin'],
-    preload:true,
-    weight: ['400', '700'],
-    display: 'swap',
-  })
--- webapp/src/app/page.js
+++
@@ -1,8 +0,0 @@
-import Homepage from "@/app/components/HomePages/Homepage";
-export default function Home() {
-  return (
-    <main>
-      <Homepage/>
-    </main>
-  );
-}
