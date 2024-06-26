9d500033439346e591f774de2c2c5112160fc229
beskay <beskay0x@gmail.com>
2024-06-26 19:35:35 +0200

    Add claimUncontestedSolution test

--- contract/test/foundry/V2_EngineV4.t.sol
+++ contract/test/foundry/V2_EngineV4.t.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "./BaseTest.sol";
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
 
-import "contracts/V2_EngineV4.sol";
 import "contracts/BaseTokenV1.sol";
+import "contracts/V2_EngineV4.sol";
 import {getIPFSCIDMemory} from "contracts/libraries/IPFS.sol";
+
+import "contracts/VotingEscrow.sol";
+import "contracts/VeNFTRender.sol";
+import "contracts/VeStaking.sol";
 
 /**
  * @notice Steps to test EngineV4:
@@ -14,7 +19,11 @@ import {getIPFSCIDMemory} from "contracts/libraries/IPFS.sol";
  * 3. Run Foundry tests with `npm run forge-test`, or `npm run forge-test-v` for verbose output
  * 3.1 Alternatively, `forge test --fork-url http://localhost:8545 --fork-block-number 18 --mc EngineV4Test`
  */
-contract EngineV4Test is BaseTest {
+contract EngineV4Test is Test {
+    VotingEscrow public votingEscrow;
+    VeStaking public veStaking;
+    VeNFTRender public veNFTRender;
+
     bytes TESTCID = "0x1220f4ad8a3bd3189da2ad909ee41148d6893d8c629c410f7f2c7e3fae75aade79c8";
     bytes TESTBUF = "0x746573740a";
 
@@ -39,8 +48,17 @@ contract EngineV4Test is BaseTest {
     function setUp() public {
         // set up is done in hardhat test file: test/enginev4.test.ts
 
-        // todo: implement this in hardhat
-        // vm.warp((1704067200  / WEEK) * WEEK); // Thu Dec 28 2023 00:00:00 GMT+0000
+        /* ve specific setup */
+        veNFTRender = new VeNFTRender();
+        votingEscrow = new VotingEscrow(address(baseToken), address(veNFTRender), address(0));
+        veStaking = new VeStaking(address(baseToken), address(votingEscrow));
+
+        // set veStaking in escrow
+        votingEscrow.setVeStaking(address(veStaking));
+
+        /* v4 specific setup */
+        vm.prank(deployer);
+        engine.setVeStaking(address(veStaking));
     }
 
     // early txs need 0 model fees and 0 task fees so they can mine tokens
@@ -126,10 +144,68 @@ contract EngineV4Test is BaseTest {
         skip(12);
         vm.roll(block.number + 1);
 
-        (uint256 staked, uint256 since, address addr) = engine.validators(validator1);
+        (uint256 staked,,) = engine.validators(validator1);
 
         vm.prank(validator1);
         engine.submitSolution(taskid, TESTCID);
+
+        (uint256 stakedAfter,,) = engine.validators(validator1);
+
+        // solution stake amount should be reserved
+        uint256 solutionsStakeAmount = engine.solutionsStakeAmount();
+        assertEq(staked - stakedAfter, solutionsStakeAmount);
+
+        // check solution val and cid
+        (address sval,,, bytes memory scid) = engine.solutions(taskid);
+        assertEq(sval, validator1);
+        assertEq(scid, TESTCID);
+
+        uint256 sStakeAmount = engine.solutionsStake(taskid);
+        assertEq(sStakeAmount, solutionsStakeAmount);
+    }
+
+    function testClaimUncontestedSolution() public {
+        deployBootstrapValidator();
+
+        bytes32 modelid = deployBootstrapModel();
+        bytes32 taskid = deployBootstrapTask(modelid, user1, 0);
+
+        bytes32 commitment = engine.generateCommitment(validator1, taskid, TESTCID);
+
+        vm.prank(validator1);
+        engine.signalCommitment(commitment);
+
+        // skip 12s and 1 block
+        skip(12);
+        vm.roll(block.number + 1);
+
+        (uint256 staked,,) = engine.validators(validator1);
+
+        vm.prank(validator1);
+        engine.submitSolution(taskid, TESTCID);
+
+        (uint256 stakedAfter,,) = engine.validators(validator1);
+
+        // solution stake amount should be reserved
+        uint256 solutionsStakeAmount = engine.solutionsStakeAmount();
+        assertEq(staked - stakedAfter, solutionsStakeAmount);
+
+        // check solution val and cid
+        (address sval,,, bytes memory scid) = engine.solutions(taskid);
+        assertEq(sval, validator1);
+        assertEq(scid, TESTCID);
+
+        uint256 sStakeAmount = engine.solutionsStake(taskid);
+        assertEq(sStakeAmount, solutionsStakeAmount);
+
+        /* claim */
+
+        // fast forward `minClaimSolutionTime`
+        skip(engine.minClaimSolutionTime() + 1);
+        vm.roll(block.number + 1);
+
+        vm.prank(validator1);
+        engine.claimSolution(taskid);
     }
 
     // todo: rewards over 7 years, ve APY, task submitting etc
