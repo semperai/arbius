baebafbb304d3a5270bed7b61298786ae1a17dda
Abhishek <munjalabhishek1996@gmail.com>
2024-06-28 12:44:25 +0530

    removed react helmet metadata

--- website/src/app/layout.js
+++ website/src/app/layout.js
@@ -2,7 +2,7 @@
 import Header from '@/app/components/Header/Header'
 import Footer from '@/app/components/Footer/Footer'
 import {Lato} from 'next/font/google'
-import { Helmet } from 'react-helmet';
+//import { Helmet } from 'react-helmet';
 
 export const lato = Lato({
     subsets: ['latin'],
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Helmet>
+        {/*<Helmet>
           <title>{metadata.title}</title>
           <meta name="description" content={metadata.description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
           <link rel="preload" as="font" href="https://res.cloudinary.com/aniket98571/raw/upload/v1715256918/Geist-SemiBold_h3w290.ttf" crossOrigin="anonymous"/>
           <link rel="preload" as="font" href="https://res.cloudinary.com/aniket98571/raw/upload/v1715232194/Geist-Regular_vvwe3i.ttf" crossOrigin="anonymous"/>
           <link rel="preload" as="font" href="https://res.cloudinary.com/aniket98571/raw/upload/v1717580903/AtHaussAero-Light_iwko9o.ttf" crossOrigin="anonymous"/>
-        </Helmet>
+        </Helmet>*/}
       </head>
       <body class="bg-white-background">
         <Header/>
