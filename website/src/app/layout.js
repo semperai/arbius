1c9460ff35779ec2952a57a18398d20b4705e814
Abhishek <munjalabhishek1996@gmail.com>
2024-06-28 10:09:46 +0530

    changed metatags data; weren't working properly earlier

--- website/src/app/layout.js
+++ website/src/app/layout.js
@@ -13,7 +13,8 @@ export const lato = Lato({
   })
 export const metadata = {
   title: "Arbius: Decentralized AI Hosting & Marketplace",
-  description: "Arbius is a decentralized network powered by GPUs globally and a shared economy around generative AI"
+  description: "Arbius is a decentralized network powered by GPUs globally and a shared economy around generative AI",
+  baseURL: "https://arbius.ai"
 };
 
 export default function RootLayout({ children }) {
@@ -26,10 +27,20 @@ export default function RootLayout({ children }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon_new.ico" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon_new.ico" />
+          
+          <meta property="og:url" content="https://arbius.ai/">
+          <meta property="og:type" content="website" />
           <meta property="og:title" content={metadata.title} />
           <meta property="og:description" content={metadata.description} />
-          <meta property="og:image" content="/arbius_thumbnail_website.png" />
-          <meta property="og:type" content="website" />
+          <meta property="og:image" content={metadata.baseURL+"/arbius_thumbnail_website.png"} />
+
+          <meta name="twitter:card" content={metadata.baseURL+"/arbius_thumbnail_website.png"}>
+          <meta property="twitter:domain" content="arbius.ai">
+          <meta property="twitter:url" content={metadata.baseURL}>
+          <meta name="twitter:title" content={metadata.title}>
+          <meta name="twitter:description" content={metadata.description}>
+          <meta name="twitter:image" content={metadata.baseURL+"/arbius_thumbnail_website.png"}>
+
           <link rel="preload" as="font" href="https://res.cloudinary.com/aniket98571/raw/upload/v1715256918/Geist-SemiBold_h3w290.ttf" crossOrigin="anonymous"/>
           <link rel="preload" as="font" href="https://res.cloudinary.com/aniket98571/raw/upload/v1715232194/Geist-Regular_vvwe3i.ttf" crossOrigin="anonymous"/>
           <link rel="preload" as="font" href="https://res.cloudinary.com/aniket98571/raw/upload/v1717580903/AtHaussAero-Light_iwko9o.ttf" crossOrigin="anonymous"/>
