1b24d90cecaa36c9a62e20dd87903b3c58e5743a
beskay <beskay0x@gmail.com>
2024-07-09 00:08:39 +0200

    Add more gov tests

--- contract/test/foundry/Governance.t.sol
+++ contract/test/foundry/Governance.t.sol
@@ -159,6 +159,130 @@ contract GovernanceTest is Test {
         assertEq(timelock.getMinDelay(), timelockMinDelay);
     }
 
+    function testGovSettings() public {
+        assertEq(governor.votingDelay(), 86400);
+        assertEq(governor.votingPeriod(), 86400 * 3);
+        assertEq(governor.proposalThreshold(), 1e18);
+    }
+
+    function testVeAiusMergeAutoDelegates() public {
+        // get votes before merge
+        uint256 votesBefore1 = votingEscrow.getVotes(address(user1));
+        uint256 votesBefore2 = votingEscrow.getVotes(address(user2));
+
+        vm.startPrank(address(user1));
+
+        // merge
+        votingEscrow.approve(address(user2), 1);
+        votingEscrow.transferFrom(address(user1), address(user2), 1);
+        vm.stopPrank();
+        vm.startPrank(address(user2));
+        votingEscrow.merge(2, 1);
+        vm.stopPrank();
+
+        // assert vote balances
+        uint256 votesAfter = votingEscrow.getVotes(address(user2));
+        assertApproxEqAbs(
+            votesBefore1 + votesBefore2,
+            votesAfter,
+            4 * 365 * 86400 // merge rounds down time lock
+        );
+    }
+
+    function testFailProposalHasQuorum() public {
+        // deployer stakes 93 AIUS for max lock time, veAIUS total supply is ~100 veAIUS
+        // quorum is 4% of total supply, so 4 veAIUS needed for successful proposal
+        vm.startPrank(deployer);
+        baseToken.approve(address(votingEscrow), 93e18);
+        votingEscrow.create_lock(93e18, 104 weeks);
+        vm.stopPrank();
+
+        // proposal
+        address[] memory targets = new address[](1);
+        targets[0] = address(baseToken);
+
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        // calldata to transfer 1 AIUS to user1
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("transfer(address,uint256)", user1, 1e18);
+
+        string memory description = "Proposal #1: Give grant to team";
+        bytes32 descriptionHash = keccak256(bytes(description));
+
+        // propose
+        vm.startPrank(user1);
+        governor.propose(targets, values, calldatas, description);
+
+        uint256 proposalId = governor.hashProposal(targets, values, calldatas, descriptionHash);
+
+        // fast forward one day (initialVotingDelay)
+        skip(1 days + 1);
+
+        // user1 votes for, but proposal should fail due to lack of quorum
+        governor.castVote(proposalId, 1);
+        
+        // queue
+        governor.queue(targets, values, calldatas, descriptionHash);
+    }
+
+    function testProposalHasQuorum() public {
+        // deployer stakes 93 AIUS for max lock time, veAIUS total supply is ~100 veAIUS
+        // quorum is 4% of total supply, so 4 veAIUS needed for successful proposal
+        vm.startPrank(deployer);
+        baseToken.approve(address(votingEscrow), 93e18);
+        votingEscrow.create_lock(93e18, 104 weeks);
+        vm.stopPrank();
+
+        // proposal
+        address[] memory targets = new address[](1);
+        targets[0] = address(baseToken);
+
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        // calldata to transfer 1 AIUS to user1
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("transfer(address,uint256)", user1, 1e18);
+
+        string memory description = "Proposal #1: Give grant to team";
+        bytes32 descriptionHash = keccak256(bytes(description));
+
+        // propose
+        vm.prank(user1);
+        governor.propose(targets, values, calldatas, description);
+
+        uint256 proposalId = governor.hashProposal(targets, values, calldatas, descriptionHash);
+
+        // fast forward one day (initialVotingDelay)
+        skip(1 days + 1);
+
+        // user1 and validator1 votes for: votes > quorum 
+        vm.prank(user1);
+        governor.castVote(proposalId, 1);
+        vm.prank(validator1);
+        governor.castVote(proposalId, 1);
+        
+        // fast forward 3 days (votingPeriod)
+        skip(3 days);
+
+        // queue
+        vm.prank(user1);
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        // fast forward timelockMinDelay
+        skip(3 days);
+
+        // execute
+        vm.prank(user1);
+        governor.execute(targets, values, calldatas, descriptionHash);
+
+        // user1 should have received 1 AIUS from timelock
+        assertEq(baseToken.balanceOf(user1), 1e18);
+        assertEq(baseToken.balanceOf(address(timelock)), 0);
+    }
+
     function testTreasuryVote() public {
         address[] memory targets = new address[](1);
         targets[0] = address(baseToken);
@@ -405,6 +529,82 @@ contract GovernanceTest is Test {
         governor.execute(targets, values, calldatas, descriptionHash);
     }
 
+    function testProposalThreshold() public {
+        // proposal
+        address[] memory targets = new address[](1);
+        targets[0] = address(baseToken);
+
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        // calldata to transfer 1 AIUS to user1
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("transfer(address,uint256)", user1, 1e18);
+
+        string memory description = "Proposal #1: Give grant to team";
+        bytes32 descriptionHash = keccak256(bytes(description));
+
+        // deployer cant propose since he has no veAIUS
+        vm.prank(deployer);
+        vm.expectRevert(abi.encodePacked("Governor: proposer votes below proposal threshold"));
+        governor.propose(targets, values, calldatas, description);
+
+        // user1 can propose since he has veAIUS
+        vm.prank(user1);
+        governor.propose(targets, values, calldatas, description);
+    }
+
+    function testVoteWithoutVeAIUS() public {
+        // proposal
+        address[] memory targets = new address[](1);
+        targets[0] = address(baseToken);
+
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        // calldata to transfer 1 AIUS to user1
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("transfer(address,uint256)", user1, 1e18);
+
+        string memory description = "Proposal #1: Give grant to team";
+        bytes32 descriptionHash = keccak256(bytes(description));
+
+        // propose
+        vm.prank(user1);
+        governor.propose(targets, values, calldatas, description);
+
+        uint256 proposalId = governor.hashProposal(targets, values, calldatas, descriptionHash);
+
+        // fast forward one day (initialVotingDelay)
+        skip(1 days + 1);
+
+        // deployer votes with zero veAIUS but a lot of AIUS, weight should be zero
+        vm.prank(deployer);
+        governor.castVote(proposalId, 1);
+
+        // fast forward 3 days (votingPeriod)
+        skip(3 days);
+
+        // proposal should fail
+        vm.prank(user1);
+        vm.expectRevert(abi.encodePacked("Governor: proposal not successful"));
+        governor.queue(targets, values, calldatas, descriptionHash);
+    }
+
+    function testAutoDelegate() public {
+        // should auto delegate to self
+        assertEq(votingEscrow.delegates(user1), user1);
+        assertEq(votingEscrow.delegates(user2), user2);
+        assertEq(votingEscrow.delegates(validator1), validator1);
+
+        // user1 delegates to user2
+        vm.prank(user1);
+        votingEscrow.delegate(user2);
+
+        // now user1 delegates to user2
+        assertEq(votingEscrow.delegates(user1), user2);
+    }
+    
     function testDelegate() public {
         // validator1 delegates his veAIUS to validator2 (val2 has ZERO veAIUS)
         vm.prank(validator1);
