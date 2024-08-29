c51e618d0dea8e2ab2604d45928d2f6c45d44e18
Kasumi Null <kasumi.null@yandex.com>
2024-08-29 22:15:14 +0800

    fix console.log in deploy testnet token script

--- contract/scripts/deploy-testnet-token.ts
+++ contract/scripts/deploy-testnet-token.ts
@@ -5,12 +5,12 @@ async function main() {
   const signers = await ethers.getSigners();
   const deployer   = signers[0];
 
+  console.log("Deploying contracts with the account:", deployer.address);
+
   const TestnetToken = await ethers.getContractFactory("TestnetToken");
   const testnetToken = await upgrades.deployProxy(TestnetToken, []);
   await testnetToken.deployed();
   console.log(`TestnetToken is deployed at ${testnetToken.address}`);
-
-  console.log("Deploying contracts with the account:", deployer.address);
 
   process.exit(0)
 }
