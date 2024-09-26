b4db352fcd8b132e78da714f8c13f6a959ebe2b8
Kasumi Null <kasumi.null@yandex.com>
2024-09-26 16:36:49 +0800

    add withdraw method and checks to v3withdraw deploy script

--- contract/scripts/010-v2-upgrade-engine-to-v3-withdraw.ts
+++ contract/scripts/010-v2-upgrade-engine-to-v3-withdraw.ts
@@ -1,5 +1,6 @@
 import { ethers, upgrades } from "hardhat";
 import { V2EngineV3 as Engine } from '../typechain/V2EngineV3';
+import { BaseTokenV1 as BaseToken } from '../typechain/BaseTokenV1';
 import * as fs from 'fs'
 import Config from './config.json';
 
@@ -16,6 +17,34 @@ async function main() {
     call: "initialize",
   });
   console.log("Engine upgraded");
+  // console.log(`attaching engine to ${Config.v2_engineAddress}`);
+  //cconst engine = await EngineV3Withdraw.attach(Config.v2_engineAddress);
+
+
+  const treasury = await engine.treasury();
+  console.log(`Treasury is ${treasury}`);
+
+  const BaseToken = await ethers.getContractFactory("BaseTokenV1");
+  // console.log(`attaching baseToken to ${Config.v2_baseTokenAddress}`);
+  const baseToken = await BaseToken.attach(Config.v2_baseTokenAddress);
+
+  {
+    const balanceBeforeEngine = await baseToken.balanceOf(engine.address);
+    console.log(`${balanceBeforeEngine} AIUS in engine`);
+    const balanceBeforeTreasury = await baseToken.balanceOf(treasury);
+    console.log(`${balanceBeforeTreasury} AIUS in treasury`);
+  }
+
+  const tx = await engine.withdrawAiusToTreasury();
+  const receipt = await tx.wait();
+  console.log(`Withdrew aius to treasury in ${receipt.transactionHash}`);
+
+  {
+    const balanceAfterEngine = await baseToken.balanceOf(engine.address);
+    console.log(`${balanceAfterEngine} AIUS in engine after withdraw`);
+    const balanceAfterTreasury = await baseToken.balanceOf(treasury);
+    console.log(`${balanceAfterTreasury} AIUS in treasury after withdraw`);
+  }
 
   process.exit(0)
 }
