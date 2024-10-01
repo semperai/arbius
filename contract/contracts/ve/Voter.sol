a11f3f9c5e0911f02e71e4be94937359fb69f00d
beskay <beskay0x@gmail.com>
2024-10-01 23:33:54 +0200

    Clean up functions, formatting

--- contract/contracts/ve/Voter.sol
+++ contract/contracts/ve/Voter.sol
@@ -1,21 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-/*
-import 'contracts/libraries/Math.sol';
-import 'contracts/interfaces/IBribe.sol';
-import 'contracts/interfaces/IBribeFactory.sol';
-import 'contracts/interfaces/IGauge.sol';
-import 'contracts/interfaces/IGaugeFactory.sol';
-import 'contracts/interfaces/IERC20.sol';
-import 'contracts/interfaces/IMinter.sol';
-import 'contracts/interfaces/IPair.sol';
-import 'contracts/interfaces/IPairFactory.sol';
-import 'contracts/interfaces/IVoter.sol';
-import 'contracts/interfaces/IVotingEscrow.sol';
+import "contracts/libraries/Math.sol";
+import "contracts/interfaces/IBribe.sol";
+import "contracts/interfaces/IBribeFactory.sol";
+import "contracts/interfaces/IGauge.sol";
+import "contracts/interfaces/IGaugeFactory.sol";
+import "contracts/interfaces/IERC20.sol";
+import "contracts/interfaces/IMinter.sol";
+import "contracts/interfaces/IPair.sol";
+import "contracts/interfaces/IPairFactory.sol";
+import "contracts/interfaces/IVoter.sol";
+import "contracts/interfaces/IVotingEscrow.sol";
 
 contract Voter is IVoter {
-
     address public immutable _ve; // the ve token that governs these contracts
     address public immutable factory; // the PairFactory
     address internal immutable base;
@@ -36,26 +34,31 @@ contract Voter is IVoter {
     mapping(address => uint256) public weights; // pool => weight
     mapping(uint => mapping(address => uint256)) public votes; // nft => pool => votes
     mapping(uint => address[]) public poolVote; // nft => pools
-    mapping(uint => uint) public usedWeights;  // nft => total voting weight of user
+    mapping(uint => uint) public usedWeights; // nft => total voting weight of user
     mapping(uint => uint) public lastVoted; // nft => timestamp of last vote, to ensure one vote per epoch
     mapping(address => bool) public isGauge;
     mapping(address => bool) public isWhitelisted;
     mapping(address => bool) public isAlive;
 
-    event GaugeCreated(address indexed gauge, address creator, address internal_bribe, address indexed external_bribe, address indexed pool);
+    event GaugeCreated(
+        address indexed gauge,
+        address creator,
+        address internal_bribe,
+        address indexed external_bribe,
+        address indexed pool
+    );
     event GaugeKilled(address indexed gauge);
     event GaugeRevived(address indexed gauge);
     event Voted(address indexed voter, uint tokenId, uint256 weight);
     event Abstained(uint tokenId, uint256 weight);
-    event Deposit(address indexed lp, address indexed gauge, uint tokenId, uint amount);
-    event Withdraw(address indexed lp, address indexed gauge, uint tokenId, uint amount);
-    event NotifyReward(address indexed sender, address indexed reward, uint amount);
-    event DistributeReward(address indexed sender, address indexed gauge, uint amount);
-    event Attach(address indexed owner, address indexed gauge, uint tokenId);
-    event Detach(address indexed owner, address indexed gauge, uint tokenId);
     event Whitelisted(address indexed whitelister, address indexed token);
 
-    constructor(address __ve, address _factory, address  _gauges, address _bribes) {
+    constructor(
+        address __ve,
+        address _factory,
+        address _gauges,
+        address _bribes
+    ) {
         _ve = __ve;
         factory = _factory;
         base = IVotingEscrow(__ve).token();
@@ -75,9 +78,13 @@ contract Voter is IVoter {
         _unlocked = 1;
     }
 
+    // todo: align this with epoch, or get epoch from votingescrow
     modifier onlyNewEpoch(uint _tokenId) {
-        // ensure new epoch since last vote 
-        require((block.timestamp / DURATION) * DURATION > lastVoted[_tokenId], "TOKEN_ALREADY_VOTED_THIS_EPOCH");
+        // ensure new epoch since last vote
+        require(
+            (block.timestamp / DURATION) * DURATION > lastVoted[_tokenId],
+            "TOKEN_ALREADY_VOTED_THIS_EPOCH"
+        );
         _;
     }
 
@@ -99,7 +106,7 @@ contract Voter is IVoter {
         emergencyCouncil = _council;
     }
 
-    /// @notice Called by users to reset voting state. 
+    /// @notice Called by users to reset voting state.
     /// @param _tokenId Id of veNFT you are reseting.
     function reset(uint _tokenId) external onlyNewEpoch(_tokenId) {
         require(IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId));
@@ -113,7 +120,7 @@ contract Voter is IVoter {
         uint _poolVoteCnt = _poolVote.length;
         uint256 _totalWeight = 0;
 
-        for (uint i = 0; i < _poolVoteCnt; i ++) {
+        for (uint i = 0; i < _poolVoteCnt; i++) {
             address _pool = _poolVote[i];
             uint256 _votes = votes[_tokenId][_pool];
 
@@ -122,8 +129,14 @@ contract Voter is IVoter {
                 weights[_pool] -= _votes;
                 votes[_tokenId][_pool] -= _votes;
                 if (_votes > 0) {
-                    IBribe(internal_bribes[gauges[_pool]])._withdraw(uint256(_votes), _tokenId);
-                    IBribe(external_bribes[gauges[_pool]])._withdraw(uint256(_votes), _tokenId);
+                    IBribe(internal_bribes[gauges[_pool]])._withdraw(
+                        uint256(_votes),
+                        _tokenId
+                    );
+                    IBribe(external_bribes[gauges[_pool]])._withdraw(
+                        uint256(_votes),
+                        _tokenId
+                    );
                     _totalWeight += _votes;
                 } else {
                     _totalWeight -= _votes;
@@ -137,20 +150,24 @@ contract Voter is IVoter {
     }
 
     /// @notice Called by users to update voting balances in voting rewards contracts.
-    /// @param _tokenId Id of veNFT whose balance you wish to update.    
+    /// @param _tokenId Id of veNFT whose balance you wish to update.
     function poke(uint _tokenId) external {
         address[] memory _poolVote = poolVote[_tokenId];
         uint _poolCnt = _poolVote.length;
         uint256[] memory _weights = new uint256[](_poolCnt);
 
-        for (uint i = 0; i < _poolCnt; i ++) {
+        for (uint i = 0; i < _poolCnt; i++) {
             _weights[i] = votes[_tokenId][_poolVote[i]];
         }
 
         _vote(_tokenId, _poolVote, _weights);
     }
 
-    function _vote(uint _tokenId, address[] memory _poolVote, uint256[] memory _weights) internal {
+    function _vote(
+        uint _tokenId,
+        address[] memory _poolVote,
+        uint256[] memory _weights
+    ) internal {
         _reset(_tokenId);
         uint _poolCnt = _poolVote.length;
         uint256 _weight = IVotingEscrow(_ve).balanceOfNFT(_tokenId);
@@ -167,7 +184,8 @@ contract Voter is IVoter {
             address _gauge = gauges[_pool];
 
             if (isGauge[_gauge]) {
-                uint256 _poolWeight = _weights[i] * _weight / _totalVoteWeight;
+                uint256 _poolWeight = (_weights[i] * _weight) /
+                    _totalVoteWeight;
                 require(votes[_tokenId][_pool] == 0);
                 require(_poolWeight != 0);
                 _updateFor(_gauge);
@@ -176,8 +194,14 @@ contract Voter is IVoter {
 
                 weights[_pool] += _poolWeight;
                 votes[_tokenId][_pool] += _poolWeight;
-                IBribe(internal_bribes[_gauge])._deposit(uint256(_poolWeight), _tokenId);
-                IBribe(external_bribes[_gauge])._deposit(uint256(_poolWeight), _tokenId);
+                IBribe(internal_bribes[_gauge])._deposit(
+                    uint256(_poolWeight),
+                    _tokenId
+                );
+                IBribe(external_bribes[_gauge])._deposit(
+                    uint256(_poolWeight),
+                    _tokenId
+                );
                 _usedWeight += _poolWeight;
                 _totalWeight += _poolWeight;
                 emit Voted(msg.sender, _tokenId, _poolWeight);
@@ -192,8 +216,12 @@ contract Voter is IVoter {
     /// @dev Weights are distributed proportional to the sum of the weights in the array.
     /// @param _tokenId     Id of veNFT you are voting with.
     /// @param _poolVote    Array of pools you are voting for.
-    /// @param _weights     Weights of pools.    
-    function vote(uint tokenId, address[] calldata _poolVote, uint256[] calldata _weights) external onlyNewEpoch(tokenId) {
+    /// @param _weights     Weights of pools.
+    function vote(
+        uint tokenId,
+        address[] calldata _poolVote,
+        uint256[] calldata _weights
+    ) external onlyNewEpoch(tokenId) {
         require(IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, tokenId));
         require(_poolVote.length == _weights.length);
         lastVoted[tokenId] = block.timestamp;
@@ -211,9 +239,9 @@ contract Voter is IVoter {
         emit Whitelisted(msg.sender, _token);
     }
 
-    /// @notice Create a new gauge 
+    /// @notice Create a new gauge
     /// @dev Governor can create a new gauge for a pool with any address.
-    /// @param _pool .    
+    /// @param _pool .
     function createGauge(address _pool) external returns (address) {
         require(gauges[_pool] == address(0x0), "exists");
         address[] memory allowedRewards = new address[](3);
@@ -230,18 +258,31 @@ contract Voter is IVoter {
             internalRewards[1] = tokenB;
 
             if (base != tokenA && base != tokenB) {
-              allowedRewards[2] = base;
+                allowedRewards[2] = base;
             }
         }
 
-        if (msg.sender != governor) { // gov can create for any pool, even non-Velodrome pairs
+        if (msg.sender != governor) {
+            // gov can create for any pool, even non-Velodrome pairs
             require(isPair, "!_pool");
-            require(isWhitelisted[tokenA] && isWhitelisted[tokenB], "!whitelisted");
+            require(
+                isWhitelisted[tokenA] && isWhitelisted[tokenB],
+                "!whitelisted"
+            );
         }
 
-        address _internal_bribe = IBribeFactory(bribefactory).createInternalBribe(internalRewards);
-        address _external_bribe = IBribeFactory(bribefactory).createExternalBribe(allowedRewards);
-        address _gauge = IGaugeFactory(gaugefactory).createGauge(_pool, _internal_bribe, _external_bribe, _ve, isPair, allowedRewards);
+        address _internal_bribe = IBribeFactory(bribefactory)
+            .createInternalBribe(internalRewards);
+        address _external_bribe = IBribeFactory(bribefactory)
+            .createExternalBribe(allowedRewards);
+        address _gauge = IGaugeFactory(gaugefactory).createGauge(
+            _pool,
+            _internal_bribe,
+            _external_bribe,
+            _ve,
+            isPair,
+            allowedRewards
+        );
 
         IERC20(base).approve(_gauge, type(uint).max);
         internal_bribes[_gauge] = _internal_bribe;
@@ -252,7 +293,13 @@ contract Voter is IVoter {
         isAlive[_gauge] = true;
         _updateFor(_gauge);
         pools.push(_pool);
-        emit GaugeCreated(_gauge, msg.sender, _internal_bribe, _external_bribe, _pool);
+        emit GaugeCreated(
+            _gauge,
+            msg.sender,
+            _internal_bribe,
+            _external_bribe,
+            _pool
+        );
         return _gauge;
     }
 
@@ -272,7 +319,7 @@ contract Voter is IVoter {
     /// @notice Revives a killed gauge. Gauge will can receive emissions and deposits again.
     /// @dev Throws if not called by emergency council.
     ///      Throws if gauge is not killed.
-    /// @param _gauge .    
+    /// @param _gauge .
     function reviveGauge(address _gauge) external {
         require(msg.sender == emergencyCouncil, "not emergency council");
         require(!isAlive[_gauge], "gauge already alive");
@@ -280,178 +327,7 @@ contract Voter is IVoter {
         emit GaugeRevived(_gauge);
     }
 
-    function attachTokenToGauge(uint tokenId, address account) external {
-        require(isGauge[msg.sender]);
-        require(isAlive[msg.sender]); // killed gauges cannot attach tokens to themselves
-        if (tokenId > 0) IVotingEscrow(_ve).attach(tokenId);
-        emit Attach(account, msg.sender, tokenId);
-    }
-
-    function emitDeposit(uint tokenId, address account, uint amount) external {
-        require(isGauge[msg.sender]);
-        require(isAlive[msg.sender]);
-        emit Deposit(account, msg.sender, tokenId, amount);
-    }
-
-    function detachTokenFromGauge(uint tokenId, address account) external {
-        require(isGauge[msg.sender]);
-        if (tokenId > 0) IVotingEscrow(_ve).detach(tokenId);
-        emit Detach(account, msg.sender, tokenId);
-    }
-
-    function emitWithdraw(uint tokenId, address account, uint amount) external {
-        require(isGauge[msg.sender]);
-        emit Withdraw(account, msg.sender, tokenId, amount);
-    }
-
     function length() external view returns (uint) {
         return pools.length;
     }
-
-    uint internal index;
-    mapping(address => uint) internal supplyIndex;
-    mapping(address => uint) public claimable;
-
-    /// @notice Called by Minter to distribute weekly emissions rewards for disbursement amongst gauges.
-    /// @dev Assumes totalWeight != 0 (Will never be zero as long as users are voting).
-    /// @param _amount Amount of rewards to distribute.
-    function notifyRewardAmount(uint amount) external {
-        _safeTransferFrom(base, msg.sender, address(this), amount); // transfer the distro in
-        uint256 _ratio = amount * 1e18 / totalWeight; // 1e18 adjustment is removed during claim
-        if (_ratio > 0) {
-            index += _ratio;
-        }
-        emit NotifyReward(msg.sender, base, amount);
-    }
-
-    /// @dev Update claims to emissions for an array of gauges.
-    /// @dev Emissions are based on the gauge weights
-    /// @param _gauges Array of gauges to update emissions for.
-    function updateFor(address[] memory _gauges) external {
-        for (uint i = 0; i < _gauges.length; i++) {
-            _updateFor(_gauges[i]);
-        }
-    }
-
-    /// @dev Update claims to emissions for gauges based on their pool id as stored in Voter.
-    /// @param start   Starting index of pools.
-    /// @param end     Ending index of pools.
-    function updateForRange(uint start, uint end) public {
-        for (uint i = start; i < end; i++) {
-            _updateFor(gauges[pools[i]]);
-        }
-    }
-
-    function updateAll() external {
-        updateForRange(0, pools.length);
-    }
-
-    /// @dev Update claims to emissions for single gauge
-    /// @param _gauge .
-    function updateGauge(address _gauge) external {
-        _updateFor(_gauge);
-    }
-
-    function _updateFor(address _gauge) internal {
-        address _pool = poolForGauge[_gauge];
-        uint256 _supplied = weights[_pool];
-        if (_supplied > 0) {
-            uint _supplyIndex = supplyIndex[_gauge];
-            uint _index = index; // get global index0 for accumulated distro
-            supplyIndex[_gauge] = _index; // update _gauge current position to global position
-            uint _delta = _index - _supplyIndex; // see if there is any difference that need to be accrued
-            if (_delta > 0) {
-                uint _share = uint(_supplied) * _delta / 1e18; // add accrued difference for each supplied token
-                if (isAlive[_gauge]) {
-                    claimable[_gauge] += _share;
-                }
-            }
-        } else {
-            supplyIndex[_gauge] = index; // new users are set to the default global state
-        }
-    }
-
-    /// @notice Claim emissions from gauges.
-    /// @param _gauges Array of gauges to collect emissions from.
-    function claimRewards(address[] memory _gauges, address[][] memory _tokens) external {
-        for (uint i = 0; i < _gauges.length; i++) {
-            IGauge(_gauges[i]).getReward(msg.sender, _tokens[i]);
-        }
-    }
-
-    /// @notice Claim bribes for a given NFT.
-    /// @dev Utility to help batch bribe claims.
-    /// @param _bribes  Array of BribeVotingReward contracts to collect from.
-    /// @param _tokens  Array of tokens that are used as bribes.
-    /// @param _tokenId Id of veNFT that you wish to claim bribes for.
-    function claimBribes(address[] memory _bribes, address[][] memory _tokens, uint _tokenId) external {
-        require(IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId));
-        for (uint i = 0; i < _bribes.length; i++) {
-            IBribe(_bribes[i]).getRewardForOwner(_tokenId, _tokens[i]);
-        }
-    }
-
-    /// @notice Claim fees for a given NFT.
-    /// @dev Utility to help batch fee claims.
-    /// @param _fees    Array of FeesVotingReward contracts to collect from.
-    /// @param _tokens  Array of tokens that are used as fees.
-    /// @param _tokenId Id of veNFT that you wish to claim fees for.
-    function claimFees(address[] memory _fees, address[][] memory _tokens, uint _tokenId) external {
-        require(IVotingEscrow(_ve).isApprovedOrOwner(msg.sender, _tokenId));
-        for (uint i = 0; i < _fees.length; i++) {
-            IBribe(_fees[i]).getRewardForOwner(_tokenId, _tokens[i]);
-        }
-    }
-
-    function distributeFees(address[] memory _gauges) external {
-        for (uint i = 0; i < _gauges.length; i++) {
-            if (IGauge(_gauges[i]).isForPair()){
-                IGauge(_gauges[i]).claimFees();
-            }
-        }
-    }
-
-    function distribute(address _gauge) public lock {
-        IMinter(minter).update_period();
-        _updateFor(_gauge); // should set claimable to 0 if killed
-        uint _claimable = claimable[_gauge];
-        if (_claimable > IGauge(_gauge).left(base) && _claimable / DURATION > 0) {
-            claimable[_gauge] = 0;
-            IGauge(_gauge).notifyRewardAmount(base, _claimable);
-            emit DistributeReward(msg.sender, _gauge, _claimable);
-        }
-    }
-
-    function distro() external {
-        distribute(0, pools.length);
-    }
-
-    function distribute() external {
-        distribute(0, pools.length);
-    }
-
-    /// @dev Utility to distribute to gauges of pools in range _start to _finish.
-    /// @param _start   Starting index of gauges to distribute to.
-    /// @param _finish  Ending index of gauges to distribute to.
-    function distribute(uint start, uint finish) public {
-        for (uint x = start; x < finish; x++) {
-            distribute(gauges[pools[x]]);
-        }
-    }
-
-    /// @dev Utility to distribute to gauges of pools in array.
-    /// @param _gauges Array of gauges to distribute to.    
-    function distribute(address[] memory _gauges) external {
-        for (uint x = 0; x < _gauges.length; x++) {
-            distribute(_gauges[x]);
-        }
-    }
-
-    function _safeTransferFrom(address token, address from, address to, uint256 value) internal {
-        require(token.code.length > 0);
-        (bool success, bytes memory data) =
-        token.call(abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))));
-    }
 }
- */
