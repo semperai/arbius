b1fb8cfb24925560008084b87490003b096df465
beskay <beskay0x@gmail.com>
2024-10-09 23:07:22 +0200

    Update voter interface

--- contract/contracts/interfaces/IVoter.sol
+++ contract/contracts/interfaces/IVoter.sol
@@ -2,55 +2,43 @@
 pragma solidity ^0.8.19;
 
 interface IVoter {
-    function createGauge(address _pool) external returns (address);
+    function createGauge(bytes32 _model) external;
 
-    function gauges(address) external view returns (address);
+    function isAlive(bytes32) external view returns (bool);
 
-    function isAlive(address) external view returns (bool);
+    function isWhitelisted(bytes32) external view returns (bool);
 
-    function isGauge(address) external view returns (bool);
-
-    function isWhitelisted(address) external view returns (bool);
-
-    function killGauge(address _gauge) external;
+    function killGauge(bytes32 _model) external;
 
     function lastVoted(uint256) external view returns (uint256);
 
     function length() external view returns (uint256);
 
-    function owner() external view returns (address);
+    function modelVote(uint256, uint256) external view returns (bytes32);
+
+    function models(uint256) external view returns (bytes32);
 
     function poke(uint256 _tokenId) external;
 
-    function poolForGauge(address) external view returns (address);
-
-    function poolVote(uint256, uint256) external view returns (address);
-
-    function pools(uint256) external view returns (address);
-
-    function renounceOwnership() external;
-
     function reset(uint256 _tokenId) external;
 
-    function reviveGauge(address _gauge) external;
+    function reviveGauge(bytes32 _model) external;
 
     function totalWeight() external view returns (uint256);
-
-    function transferOwnership(address newOwner) external;
 
     function usedWeights(uint256) external view returns (uint256);
 
     function vote(
         uint256 tokenId,
-        address[] memory _poolVote,
+        bytes32[] memory _modelVote,
         uint256[] memory _weights
     ) external;
 
-    function votes(uint256, address) external view returns (uint256);
+    function votes(uint256, bytes32) external view returns (uint256);
 
     function votingEscrow() external view returns (address);
 
-    function weights(address) external view returns (uint256);
+    function weights(bytes32) external view returns (uint256);
 
-    function whitelist(address _token) external;
+    function whitelist(bytes32 _model) external;
 }
