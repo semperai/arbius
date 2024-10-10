2bea9b19d81cc42b70d0e770e028cef951777cf0
beskay <beskay0x@gmail.com>
2024-10-10 22:26:13 +0200

    Optimize Voter.sol

--- contract/contracts/ve/Voter.sol
+++ contract/contracts/ve/Voter.sol
@@ -12,85 +12,89 @@ import "forge-std/console2.sol";
 contract Voter is IVoter, Ownable, Test {
     address public immutable votingEscrow; // the ve token that governs these contracts
 
-    uint internal constant DURATION = 7 days; // voting duration per epoch
+    uint256 internal constant DURATION = 1 weeks; // voting duration per epoch
 
-    uint public totalWeight; // total voting weight
+    uint256 public totalWeight; // total voting weight
     mapping(bytes32 => uint256) public weights; // model => weight
 
     bytes32[] public models; // all models viable for incentives
     mapping(bytes32 => bool) public isGauge; // model => isGauge
-    mapping(uint => mapping(bytes32 => uint256)) public votes; // nft => model => votes
-    mapping(uint => bytes32[]) public modelVote; // nft => models
-    mapping(uint => uint) public usedWeights; // nft => total voting weight of user
-    mapping(uint => uint) public lastVoted; // nft => timestamp of last vote, to ensure one vote per epoch
+    mapping(uint256 => mapping(bytes32 => uint256)) public votes; // nft => model => votes
+    mapping(uint256 => bytes32[]) public modelVote; // nft => models
+    mapping(uint256 => uint) public usedWeights; // nft => total voting weight of user
+    mapping(uint256 => uint) public lastVoted; // nft => timestamp of last vote, to ensure one vote per epoch
     mapping(bytes32 => bool) public isWhitelisted;
     mapping(bytes32 => bool) public isAlive;
 
     event GaugeCreated(address creator, bytes32 indexed model);
     event GaugeKilled(bytes32 indexed model);
     event GaugeRevived(bytes32 indexed model);
-    event Voted(address indexed voter, uint tokenId, uint256 weight);
-    event Abstained(uint tokenId, uint256 weight);
+    event Voted(
+        address indexed voter,
+        uint256 tokenId,
+        bytes32 model,
+        uint256 weight
+    );
+    event Abstained(uint256 tokenId, uint256 weight);
     event Whitelisted(address indexed whitelister, bytes32 indexed model);
 
     constructor(address _votingEscrow) Ownable() {
         votingEscrow = _votingEscrow;
     }
 
-    // todo: test that this aligns correctly with rewardsDuration in veStaking (it should)
-    modifier onlyNewEpoch(uint _tokenId) {
+    modifier onlyNewEpoch(uint256 _tokenId) {
         // ensure new epoch since last vote
         require(
             (block.timestamp / DURATION) * DURATION > lastVoted[_tokenId],
-            "TOKEN_ALREADY_VOTED_THIS_EPOCH"
+            "only new epoch"
         );
         _;
     }
 
+    function epochVoteEnd() external view returns (uint256) {
+        return ((block.timestamp / DURATION) * DURATION) + DURATION;
+    }
+
     /// @notice Called by users to reset voting state.
     /// @param _tokenId Id of veNFT you are reseting.
-    function reset(uint _tokenId) external onlyNewEpoch(_tokenId) {
+    function reset(uint256 _tokenId) external onlyNewEpoch(_tokenId) {
         require(
             IVotingEscrow(votingEscrow).isApprovedOrOwner(msg.sender, _tokenId)
         );
-        lastVoted[_tokenId] = block.timestamp;
         _reset(_tokenId);
-        IVotingEscrow(votingEscrow).abstain(_tokenId);
     }
 
-    function _reset(uint _tokenId) internal {
+    function _reset(uint256 _tokenId) internal {
         bytes32[] storage _modelVote = modelVote[_tokenId];
-        uint _modelVoteCnt = _modelVote.length;
+        uint256 _modelVoteCnt = _modelVote.length;
         uint256 _totalWeight = 0;
 
-        for (uint i = 0; i < _modelVoteCnt; i++) {
+        for (uint256 i = 0; i < _modelVoteCnt; i++) {
             bytes32 _model = _modelVote[i];
             uint256 _votes = votes[_tokenId][_model];
 
             if (_votes != 0) {
                 weights[_model] -= _votes;
-                votes[_tokenId][_model] -= _votes;
-                if (_votes > 0) {
-                    _totalWeight += _votes;
-                } else {
-                    _totalWeight -= _votes;
-                }
+                //votes[_tokenId][_model] -= _votes;
+                delete votes[_tokenId][_model];
+                _totalWeight += _votes;
                 emit Abstained(_tokenId, _votes);
             }
         }
-        totalWeight -= uint256(_totalWeight);
+        IVotingEscrow(votingEscrow).abstain(_tokenId);
+        totalWeight -= _totalWeight;
         usedWeights[_tokenId] = 0;
         delete modelVote[_tokenId];
     }
 
     /// @notice Called by users to update voting balances in voting rewards contracts.
     /// @param _tokenId Id of veNFT whose balance you wish to update.
-    function poke(uint _tokenId) external {
+    function poke(uint256 _tokenId) external {
         bytes32[] memory _modelVote = modelVote[_tokenId];
-        uint _modelCnt = _modelVote.length;
+        uint256 _modelCnt = _modelVote.length;
         uint256[] memory _weights = new uint256[](_modelCnt);
 
-        for (uint i = 0; i < _modelCnt; i++) {
+        for (uint256 i = 0; i < _modelCnt; i++) {
             _weights[i] = votes[_tokenId][_modelVote[i]];
         }
 
@@ -103,42 +107,42 @@ contract Voter is IVoter, Ownable, Test {
     /// @param _modelVote   Array of models you are voting for.
     /// @param _weights     Weights of models.
     function vote(
-        uint tokenId,
+        uint256 tokenId,
         bytes32[] calldata _modelVote,
         uint256[] calldata _weights
     ) external onlyNewEpoch(tokenId) {
         require(
             IVotingEscrow(votingEscrow).isApprovedOrOwner(msg.sender, tokenId)
         );
-        require(_modelVote.length == _weights.length);
+        require(_modelVote.length == _weights.length, "length mismatch");
         lastVoted[tokenId] = block.timestamp;
         _vote(tokenId, _modelVote, _weights);
     }
 
     function _vote(
-        uint _tokenId,
+        uint256 _tokenId,
         bytes32[] memory _modelVote,
         uint256[] memory _weights
     ) internal {
         _reset(_tokenId);
-        uint _modelCnt = _modelVote.length;
+        uint256 _modelCnt = _modelVote.length;
         uint256 _weight = IVotingEscrow(votingEscrow).balanceOfNFT(_tokenId);
         uint256 _totalVoteWeight = 0;
         uint256 _totalWeight = 0;
         uint256 _usedWeight = 0;
 
-        for (uint i = 0; i < _modelCnt; i++) {
+        for (uint256 i = 0; i < _modelCnt; i++) {
             _totalVoteWeight += _weights[i];
         }
 
-        for (uint i = 0; i < _modelCnt; i++) {
+        for (uint256 i = 0; i < _modelCnt; i++) {
             bytes32 _model = _modelVote[i];
 
-            if (isGauge[_model] && isAlive[_model]) {
+            if (isAlive[_model]) {
                 uint256 _modelWeight = (_weights[i] * _weight) /
                     _totalVoteWeight;
-                require(votes[_tokenId][_model] == 0);
-                require(_modelWeight != 0);
+                require(votes[_tokenId][_model] == 0, "nonzero votes");
+                require(_modelWeight != 0, "zero weight");
 
                 modelVote[_tokenId].push(_model);
 
@@ -148,12 +152,12 @@ contract Voter is IVoter, Ownable, Test {
                 _usedWeight += _modelWeight;
                 _totalWeight += _modelWeight;
 
-                emit Voted(msg.sender, _tokenId, _modelWeight);
+                emit Voted(msg.sender, _tokenId, _model, _modelWeight);
             }
         }
         if (_usedWeight > 0) IVotingEscrow(votingEscrow).voting(_tokenId);
-        totalWeight += uint256(_totalWeight);
-        usedWeights[_tokenId] = uint256(_usedWeight);
+        totalWeight += _totalWeight;
+        usedWeights[_tokenId] = _usedWeight;
     }
 
     function whitelist(bytes32 _model) external onlyOwner {
