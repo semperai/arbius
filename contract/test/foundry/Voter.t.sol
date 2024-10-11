d56828fb595cdaa97f6e6a2fb4d94271eda7fc56
beskay <beskay0x@gmail.com>
2024-10-11 23:54:06 +0200

    Add more tests

--- contract/test/foundry/Voter.t.sol
+++ contract/test/foundry/Voter.t.sol
@@ -141,7 +141,7 @@ contract VoterTest is BaseTest {
 
         // create lock for bob
         vm.prank(bob);
-        votingEscrow.create_lock(100 ether, YEAR);
+        votingEscrow.create_lock(100 ether, 2 * YEAR);
         assertEq(votingEscrow.ownerOf(2), bob);
 
         // create gauge for MODEL_1 and MODEL_2
@@ -152,23 +152,27 @@ contract VoterTest is BaseTest {
         bytes32[] memory modelVote = new bytes32[](2);
         uint256[] memory weights = new uint256[](2);
         modelVote[0] = MODEL_1;
-        weights[0] = 100;
+        weights[0] = 500;
         modelVote[1] = MODEL_2;
-        weights[1] = 400;
+        weights[1] = 500;
         
         // alice votes for MODEL_1 and MODEL_2
         vm.prank(alice);
         voter.vote(1, modelVote, weights);
 
         // check weights and other values
-        uint256 balance1 = votingEscrow.balanceOfNFT(1);
-        assertEq(voter.totalWeight(), balance1);
-        assertEq(voter.weights(MODEL_2), (balance1 * 4) / 5);
-        assertEq(voter.weights(MODEL_1), balance1 / 5);
-        assertEq(voter.votes(1, MODEL_2), (balance1 * 4) / 5);
-        assertEq(voter.votes(1, MODEL_1), balance1 / 5);
-        assertEq(voter.usedWeights(1), balance1);
+        uint256 balance = votingEscrow.balanceOfNFT(1);
+        assertEq(voter.totalWeight(), balance);
+        assertEq(voter.weights(MODEL_2), balance / 2);
+        assertEq(voter.weights(MODEL_1), balance / 2);
+        assertEq(voter.votes(1, MODEL_2), balance / 2);
+        assertEq(voter.votes(1, MODEL_1), balance / 2);
+        assertEq(voter.usedWeights(1), balance);
         assertEq(voter.lastVoted(1), block.timestamp);
+        assertEq(votingEscrow.voted(1), true);
+
+        assertEq(voter.getGaugeMultiplier(MODEL_1), 1e18 / 2);
+        assertEq(voter.getGaugeMultiplier(MODEL_2), 1e18 / 2);
 
         // bob votes for MODEL_1
         modelVote = new bytes32[](1);
@@ -180,13 +184,16 @@ contract VoterTest is BaseTest {
         voter.vote(2, modelVote, weights);
 
         // check weights and other values
-        uint256 balance2 = votingEscrow.balanceOfNFT(2);
-        assertEq(voter.totalWeight(), balance1 + balance2);
-        assertEq(voter.weights(MODEL_2), (balance1 * 4) / 5);
-        assertEq(voter.weights(MODEL_1), (balance1 / 5) + balance2);
-        assertEq(voter.votes(2, MODEL_1), balance2);
-        assertEq(voter.usedWeights(2), balance2);
+        assertEq(voter.totalWeight(), 2 * balance);
+        assertEq(voter.weights(MODEL_2), balance / 2);
+        assertEq(voter.weights(MODEL_1),  balance / 2 + balance);
+        assertEq(voter.votes(2, MODEL_1), balance);
+        assertEq(voter.usedWeights(2), balance);
         assertEq(voter.lastVoted(2), block.timestamp);
+        assertEq(votingEscrow.voted(2), true);
+
+        assertEq(voter.getGaugeMultiplier(MODEL_1), (1e18 * 3) / 4);
+        assertEq(voter.getGaugeMultiplier(MODEL_2), 1e18 / 4);
     }
 
     function testCannotChangeVoteOrResetInSameEpoch() public {
@@ -292,16 +299,19 @@ contract VoterTest is BaseTest {
         assertEq(voter.votes(1, MODEL_2), 0);
         assertEq(voter.votes(1, MODEL_1), 0);
         assertEq(voter.usedWeights(1), 0);
+
+        // votingEscrow state should be reset as well
+        assertEq(votingEscrow.voted(1), false);
     }
 
-    function testEpochDuration(uint256 warp) public {
+    function testAlignedEpochDuration(uint256 warp) public {
         // warp should be less than 100 years
         vm.assume(warp <= 3155760000);
 
         // call notifyRewardAmount so rewardDuration starts
         veStaking.notifyRewardAmount(0);
 
-        // epochVoteEnd should be identical to periodFinish
+        // epochVoteEnd should be identical to periodFinish from veStaking
         assertEq(voter.epochVoteEnd(), veStaking.periodFinish());
 
         // fast forward a random amount of time
@@ -310,5 +320,66 @@ contract VoterTest is BaseTest {
         // epochVoteEnd should still be identical to periodFinish
         veStaking.notifyRewardAmount(0);
         assertEq(voter.epochVoteEnd(), veStaking.periodFinish());
+    }
+
+    function testGaugeMultiplier(uint256 weight1, uint256 weight2, uint256 weight3) public {
+        vm.assume(weight1 <= 1e18 && weight1 > 0);
+        vm.assume(weight2 <= 1e18 && weight2 > 0);
+        vm.assume(weight3 <= 1e18 && weight3 > 0);
+
+        // create gauges
+        voter.createGauge(MODEL_1);
+        voter.createGauge(MODEL_2);
+        voter.createGauge(MODEL_3);
+
+        // create lock for alice
+        vm.prank(alice);
+        votingEscrow.create_lock(100 ether, 2 * YEAR);
+        assertEq(votingEscrow.ownerOf(1), alice);
+
+        // create lock for bob
+        vm.prank(bob);
+        votingEscrow.create_lock(100 ether, 2 * YEAR);
+        assertEq(votingEscrow.ownerOf(2), bob);
+
+        // vote for MODEL_1 and MODEL_2, with weights 100 and 400 respectively
+        bytes32[] memory modelVote = new bytes32[](3);
+        uint256[] memory weights = new uint256[](3);
+        modelVote[0] = MODEL_1;
+        weights[0] = weight1;
+        modelVote[1] = MODEL_2;
+        weights[1] = weight2;
+        modelVote[2] = MODEL_3;
+        weights[2] = weight3;
+        
+        // alice votes for MODEL_1 and MODEL_2 and MODEL_3
+        vm.prank(alice);
+        voter.vote(1, modelVote, weights);
+
+        // gaugemultiplier should be below 1e18, sum of weights should be 1e18 (minus some rounding errors, accepted error 100 wei)
+        assertLt(voter.getGaugeMultiplier(MODEL_1), 1e18);
+        assertLt(voter.getGaugeMultiplier(MODEL_2), 1e18);
+        assertLt(voter.getGaugeMultiplier(MODEL_3), 1e18);
+        assertApproxEqAbs(voter.getGaugeMultiplier(MODEL_1) + voter.getGaugeMultiplier(MODEL_2) + voter.getGaugeMultiplier(MODEL_3), 1e18, 100);
+
+        // bob votes for MODEL_1 and MODEL_2 and MODEL_3
+        // we randomize votes with mulmod
+        modelVote = new bytes32[](3);
+        weights = new uint256[](3);
+        modelVote[0] = MODEL_1;
+        weights[0] = mulmod(weight1, weight2, 1e18);
+        modelVote[1] = MODEL_2;
+        weights[1] = mulmod(weight2, weight3, 1e18);
+        modelVote[2] = MODEL_3;
+        weights[2] = mulmod(weight3, weight1, 1e18);
+
+        vm.prank(bob);
+        voter.vote(2, modelVote, weights);
+
+        // gaugemultiplier should be below 1e18, sum of weights should be 1e18 (minus some rounding errors, accepted error 100 wei)
+        assertLt(voter.getGaugeMultiplier(MODEL_1), 1e18);
+        assertLt(voter.getGaugeMultiplier(MODEL_2), 1e18);
+        assertLt(voter.getGaugeMultiplier(MODEL_3), 1e18);
+        assertApproxEqAbs(voter.getGaugeMultiplier(MODEL_1) + voter.getGaugeMultiplier(MODEL_2) + voter.getGaugeMultiplier(MODEL_3), 1e18, 100);
     }
 }
