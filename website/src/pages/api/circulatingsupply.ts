ee90ef87287fe0125e439447fb01f689e664153b
Kasumi Null <kasumi.null@yandex.com>
2024-11-07 18:15:43 +0800

    update circulating supply to account for arbitrum one

--- website/src/pages/api/circulatingsupply.ts
+++ website/src/pages/api/circulatingsupply.ts
@@ -13,23 +13,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const novaProvider = new ethers.providers.JsonRpcProvider('https://nova.arbitrum.io/rpc');
   const ethProvider = new ethers.providers.JsonRpcProvider('https://eth.llamarpc.com');
+  const oneProvider = new ethers.providers.JsonRpcProvider('https://arbitrum.llamarpc.com');
 
   const novaContract = new ethers.Contract(Config.v2_baseTokenAddress, BaseTokenArtifact.abi, novaProvider);
   const ethContract = new ethers.Contract(Config.v2_baseTokenAddress, BaseTokenArtifact.abi, ethProvider);
+  const oneContract = new ethers.Contract(Config.v4_baseTokenAddress, BaseTokenArtifact.abi, oneProvider);
 
-  const engineBalance = await novaContract.balanceOf(Config.v2_engineAddress);
-  const ethConverterBalance = await ethContract.balanceOf(Config.l1OneToOneAddress);
-  const novaConverterBalance = await novaContract.balanceOf(Config.l2OneToOneAddress);
-  const daoBalance = await novaContract.balanceOf("0x1298f8a91b046d7fcbd5454cd3331ba6f4fea168");
+  const engineBalanceNova = await novaContract.balanceOf(Config.v2_engineAddress);
+  const engineBalanceOne = await oneContract.balanceOf(Config.v4_engineAddress);
+  const converterBalanceEth = await ethContract.balanceOf(Config.l1OneToOneAddress);
+  const converterBalanceNova = await novaContract.balanceOf(Config.l2OneToOneAddress);
+  const daoBalanceEth = await ethContract.balanceOf("0xF20D0ebD8223DfF22cFAf05F0549021525015577");
+  const daoBalanceOne = await oneContract.balanceOf("0xF20D0ebD8223DfF22cFAf05F0549021525015577");
   const gysrBalance = await ethContract.balanceOf("0xA8f103eEcfb619358C35F98c9372B31c64d3f4A1");
 
   const unlocked = ethers.utils.parseEther('1000000')
-    .sub(engineBalance)
-    .sub(ethConverterBalance)
-    .sub(novaConverterBalance)
-    .sub(daoBalance)
-    .sub(gysrBalance)
-    .sub(ethers.utils.parseEther('630392'));
+    .sub(engineBalanceNova)
+    .sub(engineBalanceOne)
+    .sub(converterBalanceEth)
+    .sub(converterBalanceNova)
+    .sub(daoBalanceEth)
+    .sub(daoBalanceOne)
+    .sub(gysrBalance);
 
   try {
     res.status(200).send(ethers.utils.formatEther(unlocked))
