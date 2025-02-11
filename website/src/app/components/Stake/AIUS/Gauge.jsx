4dcbfa3c913159c215f5fcedecaf36c57fdc77e1
Abhishek Munjal <13870341+AbhiKakarot@users.noreply.github.com>
2025-02-11 15:27:22 +0530

    Added timer to a separate file; Added state management for user's values

--- website/src/app/components/Stake/AIUS/Gauge.jsx
+++ website/src/app/components/Stake/AIUS/Gauge.jsx
@@ -19,11 +19,23 @@ import info_red from '../../../assets/images/info_red.png';
 import lightning from '../../../assets/images/lightning.svg';
 import lightningbulb from '../../../assets/images/lightningbulb.svg';
 import confirmvote from '../../../assets/images/confirmvote.svg';
+import votingEscrow from '../../../abis/votingEscrow.json';
+import Web3 from 'web3';
+import Config from '@/config.one.json';
+import { getTokenIDs } from '../../../Utils/gantChart/contractInteractions';
+import { useAccount, useNetwork } from 'wagmi';
+import Timer from './Timer';
+import { AIUS_wei } from '../../../Utils/constantValues';
+
 
 const getVoteStartDate = () => {
   return new Date('08/23/2024');
 };
 function Gauge() {
+  const { address, isConnected } = useAccount();
+  const { chain, chains } = useNetwork();
+  const [totalGovernancePower, setTotalGovernancePower] = useState(0);
+
   const data = [
     {
       model_name: 'Mistral-large-2407',
@@ -67,13 +79,15 @@ function Gauge() {
     },
   ];
 
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
   const [selectedModel, setSelectedModel] = useState(null);
   const [showPopUp, setShowPopUp] = useState(false);
   const [filteredData, setFilteredData] = useState(data);
   const [searchText, setSearchText] = useState('');
   const [showConfirmVote, setShowConfirmVote] = useState(false)
+  const [votingPercentage, setVotingPercentage] = useState({});
+  const [percentageLeft, setPercentageLeft] = useState(100);
 
+  console.log(votingPercentage)
   console.log('filteredData', filteredData);
   const handleSearch = (e) => {
     console.log(e.target.value);
@@ -100,32 +114,98 @@ function Gauge() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const updateModelPercentage = (modelName, percentage) => {
+    percentage = Number(percentage)
+
+    if(typeof percentage === 'number' && percentage >= 0 && Number.isInteger(percentage)){
+
+      let sum = 0;
+      let error_key = false;
+
+      for (const [key, value] of Object.entries(votingPercentage)) {
+        if(key !== modelName){
+          sum = sum + Number(value?.percentage)
+        }
+
+        if(value?.error){
+          error_key = key
+        }
+      }
+      // Case where an error has already added but not been corrected yet; so until that is corrected, no other model's percentage will be accepted
+      if(error_key && error_key !== modelName){
+        return;
+      }
+      // Case where an error has already added but not been corrected yet; so until that is corrected, no other model's percentage will be accepted
+
+
+      console.log(sum, percentage)
+      const _sum = sum;
+      sum = sum + percentage;
+      if( sum > 100 ){
+        setVotingPercentage((prevState) => ({
+          ...prevState,
+          [modelName]: {
+            "percentage": percentage,
+            "error": "You only have "+ Math.min(100, Math.abs(100 - _sum)) +"% left"
+          },
+        }));
+        return;
+      }
+      setPercentageLeft(Math.min(100, Math.abs(100 - sum)))
+
+      setVotingPercentage((prevState) => ({
+        ...prevState,
+        [modelName]: {
+          "percentage": percentage,
+          "error": false
+        },
+      }));
+    }
+  };
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+    const initialModelPercentages = data.reduce((accumulator, item) => {
+      accumulator[item.model_name] = {
+        "percentage": 0,
+        "error": null
+      };
+      return accumulator;
+    }, {});
+    setVotingPercentage(initialModelPercentages);
 
-    return () => clearInterval(timer);
-  }, []);
+    const f = async () => {
+      try{
+        const web3 = new Web3(window.ethereum);
+        
+        const votingEscrowContract = new web3.eth.Contract(
+          votingEscrow.abi,
+          Config.v4_votingEscrowAddress
+        );
 
-  function calculateTimeLeft() {
-    const sundayTimestamp = 1739664000000;// comes from contract call
-    const now = new Date().getTime(); // Current time in milliseconds
-    const difference = sundayTimestamp - now; // Difference between Sunday and now
+        const _escrowBalanceData = await votingEscrowContract.methods
+          .balanceOf(address)
+          .call();
 
-    if (difference <= 0) {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+        let tokens = await getTokenIDs(address, _escrowBalanceData);
+        let _totalGovernancePower = 0;
+
+        tokens.forEach((token) => {
+          _totalGovernancePower = _totalGovernancePower + Number(token?.balanceOfNFT);
+        });
+
+        setTotalGovernancePower(_totalGovernancePower)
+
+        console.log(_totalGovernancePower, "TGP")
+      }catch(err){
+        console.log("Error at gauge fetch:", err)
+      }
     }
 
-    return {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60),
-    };
-  }
-
+    if (address) {
+      f();
+    } else {
+    }
+  }, [address, chain?.id]);
 
   const updateTimeRemaining = (targetDate) => {
     const now = new Date();
@@ -138,6 +218,11 @@ function Gauge() {
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     setTimeRemaining({ days, hours, minutes });
   };
+
+  const getGPUsed = () => {
+    const percentageUsed = 100 - percentageLeft;
+    return (((percentageUsed/100) * totalGovernancePower) / AIUS_wei)?.toFixed(2)
+  }
 
   return (
     <div className='mx-auto w-mobile-section-width max-w-center-width py-10 text-black-text lg:w-section-width lg:py-16'>
@@ -314,7 +399,7 @@ function Gauge() {
             {/* <h1 className='xl:text-[12px] 2xl:text-[16px]'>Voting starts in {timeRemaining.days} D : {timeRemaining.hours} Hr : {timeRemaining.minutes} Min</h1> */}
 
             <div className='flex items-center justify-start gap-2'>
-              <h1 className='xl:text-[12px] 2xl:text-[16px]'>Voting starts in {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s</h1>
+              <h1 className='xl:text-[12px] 2xl:text-[16px]'>Voting starts in <Timer epochTimestamp={1739664000000} /></h1>
               {/*<Image
                 src={skeleton}
                 className='h-[20px] w-[100px] rounded-lg opacity-70 contrast-[90%] md:w-[80px] xl:w-[140px]'
@@ -337,25 +422,25 @@ function Gauge() {
         </div>
         <div className="flex gap-2 text-purple-text border-[1px] border-purple-text p-1 rounded-[10px]">
           <div className="flex items-center gap-2 p-2 bg-white-background rounded-md">
-            <Image src={lightning} className="h-[15px] w-auto mt-[2px]" alt="" /> Total Governance Power: 200
+            <Image src={lightning} className="h-[15px] w-auto mt-[2px]" alt="" /> Total Governance Power: { (Number(totalGovernancePower) / AIUS_wei)?.toFixed(2) }
           </div>
           <div className="flex flex-col gap-1 p-2 bg-white-background rounded-md">
             <div className="flex justify-between text-[12px]">
-              <div>200/200</div>
-              <div>0% left</div>
+              <div>{getGPUsed()}/{(Number(totalGovernancePower) / AIUS_wei)?.toFixed(2)}</div>
+              <div>{percentageLeft}% left</div>
             </div>
-            <div class="w-[234px] bg-gray-text rounded-full h-2">
-              <div class="bg-purple-background h-2 rounded-full w-[60%]"></div>
+            <div className="w-[234px] bg-gray-text rounded-full h-2">
+              <div className="bg-purple-background h-2 rounded-full" style={{width: (100 - percentageLeft).toString()+"%" }}></div>
             </div>
           </div>
         </div>
         <div className="absolute right-0">
-          <div className="text-aius-tabs-gray bg-[#E8E8E8] p-[10px_45px] rounded-[25px] cursor-pointer group hidden xl:block">
+          <div className={`${ percentageLeft === 0 ? "bg-black-background text-original-white" : "bg-[#E8E8E8] text-aius-tabs-gray" } p-[10px_45px] rounded-[25px] cursor-pointer group hidden xl:block`}>
             Vote
             <div className="absolute right-[-135px] top-[-42px] bg-white-background p-2 rounded-[15px] w-[130px] opacity-0 group-hover:opacity-100 transition-all duration-300">
               <Image src={lightningbulb} alt="" />
               <div className="text-[12px]">100% of user owned governance power must be contributed before proceeding.</div>
-              <div class="absolute left-[-5px] top-[58px] w-0 h-0 border-l-[5px] border-l-transparent border-b-[8px] border-[#FFF] border-r-[5px] border-r-transparent rotate-[32deg]"></div>
+              <div className="absolute left-[-5px] top-[58px] w-0 h-0 border-l-[5px] border-l-transparent border-b-[8px] border-[#FFF] border-r-[5px] border-r-transparent rotate-[32deg]"></div>
             </div>
           </div>
         </div>
@@ -366,7 +451,7 @@ function Gauge() {
           <Image src={clock_icon} className='h-4 w-4' />
           {/* <h1>Voting starts in   02 D : 13 Hr : 16 Min</h1> */}
           <div className='flex items-center justify-start gap-2'>
-            <h1 className=''>Voting starts in {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s</h1>
+            <h1 className=''>Voting starts in <Timer epochTimestamp={1739664000000} /></h1>
             {/*<Image
               src={skeleton}
               className='h-[20px] w-[100px] rounded-lg opacity-70 contrast-[90%] md:w-[80px] xl:w-[140px]'
@@ -405,7 +490,7 @@ function Gauge() {
         {filteredData?.map((item, key) => {
           return (
             <div
-              className='gauge-table-item relative my-3 flex min-w-[1000px] items-center justify-between gap-8 rounded-lg bg-white-background px-5 pb-4 pt-4 font-semibold lg:px-10'
+              className={`gauge-table-item relative my-3 flex min-w-[1000px] items-center justify-between gap-8 rounded-lg ${ votingPercentage?.[item?.model_name]?.error === false ? "bg-[#ECF7FF]" : "bg-white-background"} px-5 pb-4 pt-4 font-semibold lg:px-10`}
               key={key}
             >
               <div
@@ -464,11 +549,16 @@ function Gauge() {
                 />*/}
                 <h1>{item?.prompts}</h1> 
               </div>
-              <div className='flex justify-end w-[15%]'>
-                <div className="flex border-[1px] border-purple-text/20 rounded-[25px]">
+              <div className='flex flex-col justify-end w-[15%]'>
+                <div className={`flex border-[1px] ${ votingPercentage?.[item?.model_name]?.error ? "border-[#C71518]" : "border-purple-text/20"} rounded-[25px]`}>
                   <div className="rounded-l-[20px] p-[6px_10px] bg-purple-text/10">%</div>
-                  <input className={"rounded-r-[25px] bg-white-background w-[70px] focus:outline-none pl-2"} />
+                  <input className={"w-full rounded-r-[25px] bg-white-background w-[70px] focus:outline-none pl-2"} value={votingPercentage?.[item?.model_name]?.percentage} onChange={(e) => updateModelPercentage(item?.model_name, e.target.value)} />
                 </div>
+                {
+                  votingPercentage?.[item?.model_name]?.error ?
+                   <div className="text-[#C71518] text-[9px] mt-[2px]">{votingPercentage?.[item?.model_name]?.error}*</div>
+                  : null
+              }
               </div>
             </div>
           );
