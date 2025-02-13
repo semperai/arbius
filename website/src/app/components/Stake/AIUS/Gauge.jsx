35a62bf1e4e54e2e5024edecbbb7e699149e8188
Abhishek Munjal <13870341+AbhiKakarot@users.noreply.github.com>
2025-02-13 11:58:44 +0530

    Fixed UI issues on New Gauge page

--- website/src/app/components/Stake/AIUS/Gauge.jsx
+++ website/src/app/components/Stake/AIUS/Gauge.jsx
@@ -41,7 +41,7 @@ function Gauge({
   const data = [
     {
       model_name: 'Mistral-large-2407',
-      model_id: 'Mistral-large-2407',
+      model_id: '0x9d04df3076afee4ab86ac2e30f103ecc9dd5bea9cb70af6881d74f638183e274',
       description: 'Text Generator',
       emissions: '40%',
       prompts: '40,304',
@@ -50,7 +50,7 @@ function Gauge({
     },
     {
       model_name: 'Nemotron-4-340b',
-      model_id: 'Nemotron-4-340b',
+      model_id: '0xba6e50e1a4bfe06c48e38800c4133d25f40f0aeb4983d953fc9369fde40ef87b',
       description: 'Text Generator',
       emissions: '30%',
       prompts: '38,994',
@@ -59,7 +59,7 @@ function Gauge({
     },
     {
       model_name: 'Llama-3.1-405b',
-      model_id: 'Llama-3.1-405b',
+      model_id: '0x6c7442f4cf999d9cb907458701b6c0dc2bb9eff10bfe20add82a9917ea955a64',
       description: 'Text Generator',
       emissions: '20%',
       prompts: '32,945',
@@ -68,7 +68,7 @@ function Gauge({
     },
     {
       model_name: 'Llama-3.1-80b',
-      model_id: 'Llama-3.1-80b',
+      model_id: '0xe6a0efde928efd1b948521b7517888363c4d5d5f1272dd6ba3170726a722afd1',
       description: 'Text Generator',
       emissions: '10%',
       prompts: '10,203',
@@ -77,7 +77,7 @@ function Gauge({
     },
     {
       model_name: 'Deepseek-coder-v2',
-      model_id: 'Deepseek-coder-v2',
+      model_id: '0x83da7b4bbec9da6b1664342bae2db3c920d43e4aebe92f9d0e8e5d80492ad68c',
       description: 'Code Generator',
       emissions: '5%',
       prompts: '6049',
@@ -97,7 +97,7 @@ function Gauge({
   const [lastUserVote, setLastUserVote] = useState(0);
   const [newTokenIDs, setNewTokensIDs] = useState([]);
   const [newGovernancePower, setNewGovernancePower] = useState(0);
-  console.log(newGovernancePower, newTokenIDs, "YAY")
+
   const handleSearch = (e) => {
     console.log(e.target.value);
     setSearchText(e.target.value);
@@ -113,9 +113,6 @@ function Gauge({
   };
 
   const updateModelPercentage = (modelName, modelBytes, percentage) => {
-    if(!isConnected){
-      return;
-    }
     percentage = Number(percentage)
 
     if(typeof percentage === 'number' && percentage >= 0 && Number.isInteger(percentage)){
@@ -392,6 +389,19 @@ function Gauge({
     });
   }
 
+  const handleFocus = (e) => {
+    if (e.target.value === "0") {
+      e.target.value = ""; // Clear the input if the value is "0"
+    }
+  };
+
+  const handleBlur = (e, modelName, modelBytes) => {
+    if (e.target.value === "") {
+      e.target.value = "0"; // Reset to "0" if the input is empty
+    }
+    updateModelPercentage(modelName, modelBytes, e.target.value); // Update the parent state
+  };
+
 
   return (
     <div className='mx-auto w-mobile-section-width max-w-center-width py-10 text-black-text lg:w-section-width lg:py-16'>
@@ -402,7 +412,7 @@ function Gauge({
               <div className="relative">
                 <Image onClick={() => setShowConfirmVote(false)} className="cursor-pointer absolute right-0" src={cross_icon} alt="" />
               </div>
-              <div className="flex text-center flex-col items-center font-Geist-Regular px-2 py-4">
+              <div className="flex text-center flex-col items-center font-Geist-Regular px-2 py-4 gap-4">
                 <Image className="h-[45px] w-[45px]" src={confirmvote} alt="" />
                 <div className="text-[32px] lato-regular">Confirm Vote</div>
                 <div className="text-aius-tabs-gray mx-[5%] lato-light">
@@ -606,10 +616,10 @@ function Gauge({
         <div className="absolute right-0">
           <div onClick={percentageLeft === 0 ? ()=>setShowConfirmVote(true) : null} className={`${ percentageLeft === 0 ? "bg-black-background text-original-white hover:bg-buy-hover" : "bg-[#E8E8E8] text-aius-tabs-gray" } p-[8px_40px] rounded-[25px] cursor-pointer group hidden xl:block`}>
             Vote
-            <div className="absolute right-[-135px] top-[-42px] bg-white-background p-2 rounded-[15px] w-[130px] opacity-0 group-hover:opacity-100 transition-all duration-300">
+            <div className="absolute left-[-10px] top-[-130px] bg-white-background p-2 rounded-[15px] w-[130px] opacity-0 group-hover:opacity-100 transition-all duration-300">
               <Image src={lightningbulb} alt="" />
               <div className="text-[12px] text-aius-tabs-gray">100% of user owned governance power must be contributed before proceeding.</div>
-              <div className="absolute left-[-5px] top-[58px] w-0 h-0 border-l-[5px] border-l-transparent border-b-[8px] border-[#FFF] border-r-[5px] border-r-transparent rotate-[32deg]"></div>
+              <div className="absolute left-[62px] bottom-[-5px] w-0 h-0 border-l-[5px] border-l-transparent border-b-[8px] border-[#FFF] border-r-[5px] border-r-transparent rotate-[60deg]"></div>
             </div>
           </div>
         </div>
@@ -670,7 +680,7 @@ function Gauge({
                   <h1 className='mb-1 text-[.6rem] text-aius-tabs-gray'>
                     Model ID
                   </h1>
-                  <p className='text-[.6rem]'>{item?.model_id}</p>
+                  <p className='text-[.6rem]'>{item?.model_id.slice(0, 6)}...{item?.model_id.slice(-4)}</p>
                 </div>
                 <Image src={polygon} className='-ml-2' />
               </div>
@@ -721,7 +731,13 @@ function Gauge({
               <div className='flex flex-col justify-end w-[15%]'>
                 <div className={`flex border-[1px] ${ votingPercentage?.[item?.model_name]?.error ? "border-[#C71518]" : "border-purple-text/20"} rounded-[25px]`}>
                   <div className="rounded-l-[20px] p-[6px_10px] bg-purple-text/10">%</div>
-                  <input className={"w-full rounded-r-[25px] bg-white-background w-[70px] focus:outline-none pl-2"} value={votingPercentage?.[item?.model_name]?.percentage} onChange={(e) => updateModelPercentage(item?.model_name, item?.model_bytes, e.target.value)} />
+                  <input
+                    className={"w-full rounded-r-[25px] bg-white-background w-[70px] focus:outline-none pl-2"}
+                    value={votingPercentage?.[item?.model_name]?.percentage}
+                    onChange={(e) => updateModelPercentage(item?.model_name, item?.model_bytes, e.target.value)}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                  />
                 </div>
                 { votingPercentage?.[item?.model_name]?.error ?
                   <div className="absolute bottom-[2px] text-[#C71518] text-[9px]">{ votingPercentage?.[item?.model_name]?.error }*</div>
--- website/src/app/components/Stake/AIUS/Process.jsx
+++ website/src/app/components/Stake/AIUS/Process.jsx
@@ -5,16 +5,16 @@ export default function Process() {
       <div className='stake-box-shadow box-border flex h-auto items-center rounded-2xl bg-white-background px-8 pb-8 pt-8 text-original-black lg:h-[246px] lg:pt-10 2xl:h-[242px] 2xl:pt-8'>
         <div>
           <div>
-            <p className='lato-bold mb-4'>veAIUS Process Overview:</p>
+            <p className='font-semibold mb-4'>veAIUS Process Overview:</p>
           </div>
           <div>
-            <p className='lato-regular mb-2 !font-[350]'>
+            <p className='mb-2 !font-[350]'>
               Lock AIUS: Receive veAIUS NFTs, earning rewards.
             </p>
-            <p className='lato-regular mb-2 !font-[350]'>
+            <p className='mb-2 !font-[350]'>
               Vote: veAIUS holders vote weekly for AI models.
             </p>
-            <p className='lato-regular mb-2 !font-[350]'>
+            <p className='mb-2 !font-[350]'>
               Emission Distribution: Managed by voter gauges based on veAIUS governance power.
             </p>
           </div>
