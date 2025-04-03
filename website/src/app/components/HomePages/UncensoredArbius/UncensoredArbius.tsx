dadcbcba47e5f0c8e30d2da8ec7d8c35ef738268
Abhishek Munjal <13870341+AbhiKakarot@users.noreply.github.com>
2025-04-03 19:52:05 +0530

    Updated AI Arbius image on mobile

--- website/src/app/components/HomePages/UncensoredArbius/UncensoredArbius.tsx
+++ website/src/app/components/HomePages/UncensoredArbius/UncensoredArbius.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import UncensoredArbiusImage from '@/app/assets/images/uncensored_arbius_image.svg';
-import UncensoredArbiusMobileImage from '@/app/assets/images/uncensored_arbius_mobile_image.svg';
+import UncensoredArbiusMobileImage from '@/app/assets/images/ai_arbius_image_mobile.svg';
 import RightArrow from '@/app/assets/images/right_arrow.svg';
 import Link from 'next/link';
 import { Fade } from 'react-awesome-reveal';
