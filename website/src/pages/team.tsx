b981cabe225b7e691d53121eadd0e5c23506918f
Abhishek Munjal <13870341+AbhiKakarot@users.noreply.github.com>
2025-06-06 19:56:38 +0530

    Changed cyrus and paul's image; another minor edit

--- website/src/pages/team.tsx
+++ website/src/pages/team.tsx
@@ -113,8 +113,8 @@ export default function Team() {
               { team_data.map(function(item, index){
                 return <div key={index} className={`relative ${index === 8 ? "xl:col-start-2" : index === 9 ? "lg:col-start-2 xl:col-start-3" : ""}`}>
                         <Image src={item.image} alt={item.name} />
-                        { item.telegram ? <div className={`absolute top-[5px] lm:top-[10px] right-[5px] lm:right-[10px] flex items-center p-1 md:p-2 rounded-[15px] ${item.telegram_bg} backdrop-blur`}>
-                          <Image className="h-[15px] md:h-[18px]" src={TelegramIcon} alt="telegram" />
+                        { item.telegram ? <div className={`absolute top-[5px] lm:top-[10px] right-[5px] lm:right-[10px] flex items-center gap-[2px] p-1 md:p-2 rounded-[15px] ${item.telegram_bg} backdrop-blur`}>
+                          <Image className="h-[15px] w-auto md:h-[18px]" src={TelegramIcon} alt="telegram" />
                           <div className="text-[11px] md:text-[13px]">@{item.telegram}</div>
                         </div> : null }
                         
